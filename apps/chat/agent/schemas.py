b0af247d1f795e7075f4db61441bbe546bdcc36c
Chris Smit <chris.smit@dimagi.com>
2023-10-08 19:19:29 +0200

    Split reminders up into recurring and one-off, and send the current datetime as a system message

--- apps/chat/agent/schemas.py
+++ apps/chat/agent/schemas.py
@@ -1,14 +1,25 @@
 from datetime import datetime
+from enum import Enum
 from typing import Optional
 
 from pydantic import BaseModel, Field
 
 
-class ReminderSchema(BaseModel):
-    datetime_due: datetime = Field(description="reminder due date in ISO 8601 format")
-    repeating: bool = Field(description="indicates whether this reminder should be repeating")
-    periods: int = Field(description="For repeating reminders, this is the number of periods to repeat for")
-    interval_minutes: int = Field(
-        description="For repeating reminders, this is the interval between periods in minutes"
-    )
-    reminder_message: str = Field(description="The message of the reminder")
+class PeriodEnum(str, Enum):
+    SECOND = "seconds"
+    MINUTE = "minutes"
+    HOUR = "hours"
+    DAY = "days"
+
+
+class RecurringReminderSchema(BaseModel):
+    datetime_due: datetime = Field(description="the first (or only) reminder due date in ISO 8601 format")
+    every: int = Field(description="Number of interval periods to wait before the next reminder")
+    period: PeriodEnum = Field(description="The type of period between reminders")
+    datetime_end: datetime = Field(description="the date of the last reminder in ISO 8601 format")
+    message: str = Field(description="The reminder message")
+
+
+class OneOffReminderSchema(BaseModel):
+    datetime_due: datetime = Field(description="the datetime that the reminder is due in ISO 8601 format")
+    message: str = Field(description="The reminder message")
