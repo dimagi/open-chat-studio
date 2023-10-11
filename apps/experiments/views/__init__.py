b7ef699547cc025cce0c894f8eea9a77d22c1f62
Simon Kelly <skelly@dimagi.com>
2023-10-11 16:32:38 +0200

    split views file

---
+++ apps/experiments/views/__init__.py
@@ -0,0 +1,36 @@
+from .experiment import (  # noqa: F401
+    download_experiment_chats,
+    end_experiment,
+    experiment_chat,
+    experiment_chat_session,
+    experiment_complete,
+    experiment_invitations,
+    experiment_pre_survey,
+    experiment_review,
+    experiment_session_message,
+    experiment_session_view,
+    experiments_home,
+    get_message_response,
+    poll_messages,
+    send_invitation,
+    single_experiment_home,
+    start_experiment,
+    start_experiment_session,
+    start_session,
+)
+from .prompt import (  # noqa: F401
+    experiments_prompt_builder,
+    experiments_prompt_builder_get_message,
+    get_prompt_builder_history,
+    get_prompt_builder_message_response,
+    prompt_builder_load_prompts,
+    prompt_builder_load_source_material,
+    prompt_builder_start_save_process,
+)
+from .safety import (  # noqa: F401
+    CreateSafetyLayer,
+    EditSafetyLayer,
+    SafetyLayerTableView,
+    delete_safety_layer,
+    safety_layer_home,
+)
