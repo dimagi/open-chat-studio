9c006748d68c1180d260926dd8b9e7f173af1846
Simon Kelly <skelly@dimagi.com>
2023-10-20 14:10:20 +0200

    render default form

--- apps/llm_providers/utils.py
+++ apps/llm_providers/utils.py
@@ -21,9 +21,16 @@ def _get_main_form(instance=None, data=None):
     form_cls = forms.modelform_factory(
         LlmProvider,
         fields=["name", "type"],
+        formfield_callback=formfield_for_dbfield,
     )
     form = form_cls(data=data, instance=instance)
     if instance:
         form.fields["type"].disabled = True
 
     return form
+
+
+def formfield_for_dbfield(db_field, **kwargs):
+    if db_field.name == "type":
+        return forms.TypedChoiceField(empty_value=None, choices=LlmProviderType.choices)
+    return db_field.formfield(**kwargs)
