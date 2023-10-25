23d7f599de1fbdfe091c0e8b0ad604a06575ffaf
Simon Kelly <skelly@dimagi.com>
2023-10-25 11:10:03 +0200

    lint

--- apps/experiments/models.py
+++ apps/experiments/models.py
@@ -297,7 +297,7 @@ class Experiment(BaseTeamModel):
 
     def get_chat_model(self):
         return self.llm_provider.get_chat_model(self.llm, self.temperature)
-    
+
     def get_absolute_url(self):
         return reverse("experiments:single_experiment_home", args=[self.team.slug, self.id])
 
