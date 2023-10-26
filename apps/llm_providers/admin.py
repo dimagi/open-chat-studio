1222c32297aadffebc115fcb9c1d28b54c453cde
Simon Kelly <skelly@dimagi.com>
2023-10-26 12:21:39 +0200

    remove old llm provider code

--- apps/llm_providers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import LlmProvider
-
-
-@admin.register(LlmProvider)
-class ServiceConfigAdmin(admin.ModelAdmin):
-    list_display = ("name", "team", "type")
-    list_filter = ("team", "type")
