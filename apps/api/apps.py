98729b92cb625502b5c5b5bf81cc1b3039532b58
Simon Kelly <skelly@dimagi.com>
2023-10-31 14:54:25 +0200

    use `BigAutoField` for all models

--- apps/api/apps.py
+++ apps/api/apps.py
@@ -4,4 +4,3 @@ from django.apps import AppConfig
 class APIConfig(AppConfig):
     name = "apps.api"
     label = "api"
-    default_auto_field = "django.db.models.BigAutoField"
--- apps/experiments/apps.py
+++ apps/experiments/apps.py
@@ -4,7 +4,6 @@ from django.apps import AppConfig
 class ExperimentAppConfig(AppConfig):
     name = "apps.experiments"
     label = "experiments"
-    default_auto_field = "django.db.models.BigAutoField"
 
     def ready(self):
         from . import signals
