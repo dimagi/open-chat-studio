246a50d2b22d8420993aff415fcba0494169251d
Simon Kelly <skelly@dimagi.com>
2023-11-03 11:31:56 +0200

    don't raise 404 if team not found

--- apps/teams/helpers.py
+++ apps/teams/helpers.py
@@ -26,7 +26,10 @@ def get_next_unique_team_slug(team_name: str) -> str:
 def get_team_for_request(request, view_kwargs):
     team_slug = view_kwargs.get("team_slug", None)
     if team_slug:
-        return get_object_or_404(Team, slug=team_slug)
+        try:
+            return Team.objects.get(slug=team_slug)
+        except Team.DoesNotExist:
+            return None
 
     if not request.user.is_authenticated:
         return
