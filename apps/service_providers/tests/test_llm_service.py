964deb47fa6170f261649d6e734a6ca479b5587f
Simon Kelly <skelly@dimagi.com>
2023-11-07 11:46:18 +0200

    add api_version parameter to azure LLM service config
    Resolves https://github.com/dimagi/open-chat-studio/issues/117

--- apps/service_providers/tests/test_llm_service.py
+++ apps/service_providers/tests/test_llm_service.py
@@ -7,5 +7,7 @@ def test_open_ai_service():
 
 
 def test_azure_ai_service():
-    service = AzureLlmService(openai_api_key="test", openai_api_base="https://api.openai.com/v1")
+    service = AzureLlmService(
+        openai_api_key="test", openai_api_base="https://api.openai.com/v1", openai_api_version="v1"
+    )
     assert not service.supports_transcription
