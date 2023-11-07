e7305a2fc831b136bd3e78cb84f2806d06f5ad54
Simon Kelly <skelly@dimagi.com>
2023-11-07 18:07:29 +0200

    setup default groups
    setup default groups

--- apps/teams/backends.py
+++ apps/teams/backends.py
@@ -1,5 +1,10 @@
+import dataclasses
+import operator
+from functools import reduce
+
 from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Group, Permission
+from django.db import models
 
 from apps.teams.utils import get_current_team
 
@@ -11,3 +16,113 @@ class TeamBackend(ModelBackend):
             return Permission.objects.none()
 
         return Permission.objects.filter(group__membership__team=current_team, group__membership__user=user_obj)
+
+
+CONTENT_TYPES = {
+    "channels": ["experimentchannel"],
+    "chat": ["chat", "chatmessage"],
+    "experiments": [
+        "consentform",
+        "experiment",
+        "experimentsession",
+        "noactivitymessageconfig",
+        "participant",
+        "prompt",
+        "promptbuilderhistory",
+        "safetylayer",
+        "sourcematerial",
+        "survey",
+        "syntheticvoice",
+    ],
+    "service_providers": ["llmprovider", "voiceprovider"],
+    "teams": ["invitation", "membership", "team"],
+}
+
+VIEW = "view"
+CHANGE = "change"
+DELETE = "delete"
+ADD = "add"
+ALL = [VIEW, CHANGE, DELETE, ADD]
+
+
+@dataclasses.dataclass
+class AppPermSetDef:
+    app_label: str
+    permissions: list[str]
+
+    @property
+    def codenames(self):
+        return [f"{permission}_{model}" for model in CONTENT_TYPES[self.app_label] for permission in self.permissions]
+
+
+@dataclasses.dataclass
+class ModelPermSetDef:
+    app_label: str
+    model: str
+    permissions: list[str]
+
+    @property
+    def codenames(self):
+        return [f"{permission}_{self.model}" for permission in self.permissions]
+
+
+@dataclasses.dataclass
+class GroupDef:
+    name: str
+    permission_defs: list[ModelPermSetDef | AppPermSetDef]
+
+    def update_or_create(self):
+        group, _ = Group.objects.get_or_create(name=self.name)
+        group.permissions.set(self.get_permissions())
+        return group
+
+    def get_permissions(self):
+        filters = reduce(
+            operator.or_,
+            [
+                models.Q(content_type__app_label=perm_set.app_label, codename__in=perm_set.codenames)
+                for perm_set in self.permission_defs
+            ],
+        )
+        return Permission.objects.filter(filters)
+
+
+GROUPS = [
+    GroupDef("Super Admin", [AppPermSetDef(app_label, ALL) for app_label in CONTENT_TYPES]),
+    GroupDef(
+        "Team Admin",
+        [
+            AppPermSetDef("teams", ALL),
+            AppPermSetDef("service_providers", ALL),
+        ],
+    ),
+    GroupDef(
+        "Experiment Admin",
+        [
+            AppPermSetDef("experiments", ALL),
+            AppPermSetDef("channels", ALL),
+        ],
+    ),
+    GroupDef(
+        "Chat Viewer",
+        [
+            AppPermSetDef("chat", [VIEW]),
+        ],
+    ),
+]
+
+_groups_created = False
+
+
+def create_default_groups():
+    """
+    Creates the default groups for the team.
+    """
+    global _groups_created
+    if _groups_created:
+        return
+
+    for group_def in GROUPS:
+        group_def.update_or_create()
+
+    _groups_created = True
--- apps/teams/signals.py
+++ apps/teams/signals.py
@@ -1,7 +1,9 @@
 from allauth.account.signals import user_signed_up
 from django.core.signals import request_finished
+from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
+from .backends import create_default_groups
 from .helpers import create_default_team_for_user
 from .invitations import get_invitation_id_from_request, process_invitation
 from .models import Invitation
@@ -30,3 +32,11 @@ def add_user_to_team(request, user, **kwargs):
 @request_finished.connect
 def clear_team_context(sender, **kwargs):
     unset_current_team()
+
+
+@post_migrate.connect
+def sync_groups(sender, **kwargs):
+    """
+    Syncs the groups with the permissions.
+    """
+    create_default_groups()
