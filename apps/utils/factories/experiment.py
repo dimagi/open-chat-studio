34a75e45b97508067c5a9ae34f6a40e325a25a4d
Chris Smit <chris.smit@dimagi.com>
2023-11-13 13:46:22 +0200

    Add experiment factories and test the new method

---
+++ apps/utils/factories/experiment.py
@@ -0,0 +1,47 @@
+import factory
+
+from apps.experiments.models import ConsentForm, Experiment, Prompt, SourceMaterial
+from apps.utils.factories.team import TeamFactory
+from apps.utils.factories.user import UserFactory
+
+
+class PromptFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Prompt
+
+    owner = factory.SubFactory(UserFactory)
+    name = "Some name"
+    description = "This is a description"
+    team = factory.SubFactory(TeamFactory)
+
+
+class ConsentFormFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ConsentForm
+
+    name = "Consent form"
+    consent_text = "Do you give consent?"
+    is_default = True
+    team = factory.SubFactory(TeamFactory)
+
+
+class SourceMaterialFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = SourceMaterial
+
+    owner = factory.SubFactory(UserFactory)
+    topic = "Some source"
+    description = "Some description"
+    material = "material"
+    team = factory.SubFactory(TeamFactory)
+
+
+class ExperimentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Experiment
+
+    owner = factory.SubFactory(UserFactory)
+    name = factory.Faker("name")
+    chatbot_prompt = factory.SubFactory(PromptFactory)
+    consent_form = factory.SubFactory(ConsentFormFactory)
+    team = factory.LazyAttribute(lambda obj: obj.chatbot_prompt.team)
