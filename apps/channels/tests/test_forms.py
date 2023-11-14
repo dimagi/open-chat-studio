c395dd250a2e7941fef29fbd3869faa253cff18e
Chris Smit <chris.smit@dimagi.com>
2023-11-14 12:33:49 +0200

    Refactor test

--- apps/channels/tests/test_forms.py
+++ apps/channels/tests/test_forms.py
@@ -7,8 +7,14 @@ from apps.service_providers.models import MessagingProvider, MessagingProviderTy
 from apps.utils.factories.service_provider_factories import MessagingProviderFactory
 
 
-@pytest.mark.parametrize("platform,", ["whatsapp", "telegram"])
-def test_channel_form_reveals_provider_types(team, platform):
+@pytest.mark.parametrize(
+    "platform,expected_widget_cls",
+    [
+        ("whatsapp", Select),
+        ("telegram", HiddenInput),
+    ],
+)
+def test_channel_form_reveals_provider_types(team, platform, expected_widget_cls):
     """Test that the message provider field is being hidden when not applicable to a certain platform"""
     # First create a messaging provider
     message_provider = MessagingProviderFactory(type=MessagingProviderType("twilio"), team=team)
@@ -16,11 +22,8 @@ def test_channel_form_reveals_provider_types(team, platform):
 
     form = ChannelForm(initial={"platform": ChannelPlatform(platform)}, team=team)
     widget = form.fields["messaging_provider"].widget
-    form_queryset = form.fields["messaging_provider"].queryset
-    if platform == "whatsapp":
-        assert isinstance(widget, Select)
-    else:
-        assert isinstance(widget, HiddenInput)
+    assert isinstance(widget, expected_widget_cls)
 
+    form_queryset = form.fields["messaging_provider"].queryset
     assert form_queryset.count() == MessagingProvider.objects.filter(team=team).count()
     assert form_queryset.first() == message_provider
