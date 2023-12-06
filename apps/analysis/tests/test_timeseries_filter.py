cc841c5596eede4b55b06d51b341a19ba9ba76ae
Simon Kelly <skelly@dimagi.com>
2023-12-06 18:03:31 +0200

    timeseries filter params tests

---
+++ apps/analysis/tests/test_timeseries_filter.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+import pytest
+
+from apps.analysis.steps.filters import DurationUnit, TimeseriesFilter, TimeseriesFilterParams
+
+
+def _make_params(duration_unit, duration_value, anchor_type, anchor_point=None):
+    return TimeseriesFilterParams(
+        duration_unit=duration_unit,
+        duration_value=duration_value,
+        anchor_type=anchor_type,
+        anchor_point=datetime.fromisoformat(anchor_point or "2022-04-01"),
+    )
+
+
+@pytest.mark.parametrize(
+    "params,start,end",
+    [
+        pytest.param(
+            _make_params(DurationUnit.minutes, 1, "this"), "2022-04-01", "2022-04-01T00:01", id="minutes basic"
+        ),
+        pytest.param(
+            _make_params(DurationUnit.minutes, 1, "this", "2022-04-01T00:00:04"),
+            "2022-04-01",
+            "2022-04-01T00:01",
+            id="minutes,anchor_point rounding",
+        ),
+        pytest.param(
+            _make_params(DurationUnit.minutes, 2, "this"),
+            "2022-04-01",
+            "2022-04-01T00:02",
+            id="minutes duration value 2",
+        ),
+        pytest.param(
+            _make_params(DurationUnit.minutes, 2, "last"), "2022-03-31T23:58", "2022-04-01", id="minutes last"
+        ),
+        pytest.param(
+            _make_params(DurationUnit.minutes, 2, "last", "2022-04-01T00:00:04"),
+            "2022-03-31T23:58",
+            "2022-04-01",
+            id="minutes last,anchor_point rounding",
+        ),
+        pytest.param(_make_params(DurationUnit.hours, 1, "this"), "2022-04-01", "2022-04-01T01:00", id="hours basic"),
+        pytest.param(
+            _make_params(DurationUnit.hours, 1, "this", "2022-04-01T00:53"),
+            "2022-04-01",
+            "2022-04-01T01:00",
+            id="hours,anchor_point rounding",
+        ),
+        pytest.param(_make_params(DurationUnit.days, 1, "this"), "2022-04-01", "2022-04-02", id="days basic"),
+        pytest.param(
+            _make_params(DurationUnit.days, 1, "this", "2022-04-01T03:12"),
+            "2022-04-01",
+            "2022-04-02",
+            id="days,anchor_point rounding",
+        ),
+        pytest.param(
+            _make_params(DurationUnit.weeks, 1, "this"), "2022-03-28", "2022-04-04", id="weeks, anchor point on Monday"
+        ),
+        pytest.param(_make_params(DurationUnit.months, 1, "this"), "2022-04-01", "2022-05-01", id="months basic"),
+        pytest.param(
+            _make_params(DurationUnit.months, 1, "this", "2022-04-03"),
+            "2022-04-01",
+            "2022-05-01",
+            id="months,anchor_point rounding",
+        ),
+        pytest.param(
+            _make_params(DurationUnit.years, 1, "this"), "2022-01-01", "2023-01-01", id="years, anchor point on Jan 1"
+        ),
+    ],
+)
+def test_timeseries_filter_params(params, start, end):
+    assert params.start == datetime.fromisoformat(start)
+    assert params.end == datetime.fromisoformat(end)
