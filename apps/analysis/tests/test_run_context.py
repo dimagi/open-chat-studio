6cd1e40fe37eb827dfb45d4b3f5d8df91ab4fcc9
Simon Kelly <skelly@dimagi.com>
2023-12-07 12:28:51 +0200

    use mocking for context tests

--- apps/analysis/tests/test_run_context.py
+++ apps/analysis/tests/test_run_context.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 
 from apps.analysis.models import Analysis, AnalysisRun, RunGroup, RunStatus
@@ -28,6 +30,22 @@ def analysis(team, llm_provider):
     )
 
 
+@pytest.fixture()
+def mock_run_group():
+    analysis = Analysis(llm_provider=LlmProvider(), llm_model="test")
+    analysis.llm_provider.get_llm_service = mock.MagicMock()
+    group = RunGroup(analysis=analysis, params={})
+    group.save = mock.MagicMock()
+    return group
+
+
+@pytest.fixture()
+def mock_analysis_run(mock_run_group):
+    run = AnalysisRun(group=mock_run_group)
+    run.save = mock.MagicMock()
+    return run
+
+
 @pytest.mark.parametrize(
     "params, expected",
     [
@@ -36,70 +54,44 @@ def analysis(team, llm_provider):
         ({"foo": "bar"}, {"llm_model": "test", "foo": "bar"}),
     ],
 )
-@pytest.mark.django_db
-def test_run_context_params(params, expected, team, analysis):
-    group = RunGroup.objects.create(
-        team=team,
-        analysis=analysis,
-        params=params,
-    )
-    run = AnalysisRun.objects.create(group=group)
-    with run_context(run) as pipeline_context:
+# @pytest.mark.django_db
+def test_run_context_params(params, expected, mock_analysis_run):
+    mock_analysis_run.group.params = params
+    with run_context(mock_analysis_run) as pipeline_context:
         assert pipeline_context.params == expected
 
 
-def test_run_context(team, analysis):
-    group = RunGroup.objects.create(
-        team=team,
-        analysis=analysis,
-        params={},
-    )
-    run = AnalysisRun.objects.create(group=group)
-    with run_context(run) as pipeline_context:
-        run.refresh_from_db()
-        assert run.start_time is not None
-        assert run.status == RunStatus.RUNNING
+def test_run_context(mock_analysis_run):
+    with run_context(mock_analysis_run) as pipeline_context:
+        assert mock_analysis_run.start_time is not None
+        assert mock_analysis_run.status == RunStatus.RUNNING
         pipeline_context.log.info("test log")
         with pipeline_context.log("magic"):
             pipeline_context.log.info("test log 2")
 
-    run.refresh_from_db()
-    assert run.end_time is not None
-    assert run.status == RunStatus.SUCCESS
-    logs = [(entry["logger"], entry["message"]) for entry in run.log["entries"]]
+    assert mock_analysis_run.end_time is not None
+    assert mock_analysis_run.status == RunStatus.SUCCESS
+    logs = [(entry["logger"], entry["message"]) for entry in mock_analysis_run.log["entries"]]
     assert logs == [
         ("root", "test log"),
         ("magic", "test log 2"),
     ]
 
 
-def test_run_context_error(team, analysis):
-    group = RunGroup.objects.create(
-        team=team,
-        analysis=analysis,
-        params={},
-    )
-    run = AnalysisRun.objects.create(group=group)
+def test_run_context_error(mock_analysis_run):
     with pytest.raises(Exception):
-        with run_context(run):
+        with run_context(mock_analysis_run):
             raise Exception("test exception")
 
-    run.refresh_from_db()
-    assert run.end_time is not None
-    assert run.status == RunStatus.ERROR
-    assert run.error == """Exception('test exception')"""
+    assert mock_analysis_run.end_time is not None
+    assert mock_analysis_run.status == RunStatus.ERROR
+    assert mock_analysis_run.error == """Exception('test exception')"""
 
 
-def test_run_status_context_error(team, analysis):
-    group = RunGroup.objects.create(
-        team=team,
-        analysis=analysis,
-        params={},
-    )
-    with run_status_context(group, raise_errors=False):
+def test_run_status_context_error(mock_run_group):
+    with run_status_context(mock_run_group, raise_errors=False):
         raise Exception("test exception")
 
-    group.refresh_from_db()
-    assert group.end_time is not None
-    assert group.status == RunStatus.ERROR
-    assert group.error == """Exception('test exception')"""
+    assert mock_run_group.end_time is not None
+    assert mock_run_group.status == RunStatus.ERROR
+    assert mock_run_group.error == """Exception('test exception')"""
