d449a22beb609ae4dbf2aaab994587f7f0091dcd
Simon Kelly <skelly@dimagi.com>
2023-12-11 21:01:01 +0200

    remove unused template tag

--- apps/teams/templatetags/team_tags.py
+++ apps/teams/templatetags/team_tags.py
@@ -5,12 +5,15 @@ from apps.teams.roles import is_admin
 register = template.Library()
 
 
-@register.filter
-def is_admin_of(user, team):
-    return is_admin(user, team)
-
-
 @register.simple_tag(takes_context=True)
 def has_perm(context, app_label, permission):
+    """Template tag to check dynamic permissions:
+
+    {% load team_tags %}
+    {% has_perm app_label perm_name as has_permission %}
+    {% if has_permission %}
+        ...
+    {% endif %}
+    """
     request = context["request"]
     return request.user.has_perm(f"{app_label}.{permission}")
