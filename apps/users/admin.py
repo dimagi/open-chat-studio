91ae1dba1463b89cab999c86ba4488c25dad578b
Simon Kelly <skelly@dimagi.com>
2024-01-18 14:56:49 +0200

    update user admin for better filtering / auditing

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+from apps.utils.django_admin import RelativeDateFieldListFilter
 
 from .models import CustomUser
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = UserAdmin.list_display
-
+    list_display = UserAdmin.list_display + (
+        "last_login",
+        "last_login_natural",
+    )
+    list_filter = UserAdmin.list_filter + (("last_login", RelativeDateFieldListFilter),)
     fieldsets = UserAdmin.fieldsets + (("Custom Fields", {"fields": ("avatar", "language")}),)
+
+    @admin.display(description="Last Login")
+    def last_login_natural(self, obj):
+        return naturaltime(obj.last_login)
