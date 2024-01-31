132f660d52d56c4e788b77a8719fb9aed4bc86d1
Chris Smit <chris.smit@dimagi.com>
2024-01-31 19:49:41 +0200

    Add todos

--- apps/service_providers/messaging_service.py
+++ apps/service_providers/messaging_service.py
@@ -62,11 +62,9 @@ class TurnIOService(MessagingService):
         self.client.messages.send_text(to_number, message)
 
     def send_whatsapp_voice_message(self, media_url: str, from_number: str, to_number):
-        # self.client.messages.create(from_=f"whatsapp:{from_number}", to=f"whatsapp:{to_number}", media_url=[media_url])
+        # TODO
         pass
 
     def get_message_audio(self, url: str) -> BytesIO:
-        # auth = (self.account_sid, self.auth_token)
-        # ogg_audio = BytesIO(requests.get(url, auth=auth).content)
-        # return audio.convert_ogg_to_wav(ogg_audio)
+        # TODO
         pass
