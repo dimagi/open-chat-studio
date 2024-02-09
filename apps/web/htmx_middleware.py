82616d979776f5d0c487b098b498b4407066ded4
Simon Kelly <skelly@dimagi.com>
2024-02-09 16:04:57 +0200

    support messages for htmx requests

---
+++ apps/web/htmx_middleware.py
@@ -0,0 +1,21 @@
+import json
+
+from django.contrib.messages import get_messages
+from django.utils.deprecation import MiddlewareMixin
+
+
+class HtmxMessageMiddleware:
+    def __init__(self, get_response):
+        if get_response is None:
+            raise ValueError("get_response must be provided.")
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request.is_htmx = request.headers.get("HX-Request") == "true"
+        response = self.get_response(request)
+        if request.is_htmx:
+            response.headers["HX-Trigger"] = json.dumps(
+                {"messages": [{"message": message.message, "tags": message.tags} for message in get_messages(request)]}
+            )
+
+        return response
