e68e929a6ae9a00b25d32a4954b789699262bdf3
Chris Smit <chris.smit@dimagi.com>
2024-02-12 16:48:25 +0200

    Add generic audio conversion method

--- apps/channels/audio.py
+++ apps/channels/audio.py
@@ -18,3 +18,15 @@ def convert_audio_to_wav(audio: BytesIO, source_format="ogg") -> BytesIO:
     wav_audio.name = "some_name.wav"
 
     return wav_audio
+
+
+def convert_audio(audio: BytesIO, target_format: str, source_format="ogg") -> BytesIO:
+    audio = AudioSegment.from_file(audio, format=source_format)
+    # Convert to mono
+    audio = audio.set_channels(1)
+
+    new_audio = BytesIO()
+    audio.export(new_audio, format=target_format)
+    new_audio.seek(0)
+    new_audio.name = f"some_name.{target_format}"
+    return new_audio
