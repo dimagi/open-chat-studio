c43fe679c0781b8bc4d138e4a225161e08ded706
Simon Kelly <skelly@dimagi.com>
2024-02-12 14:57:54 +0200

    ignore unused * imports

--- apps/teams/views/__init__.py
+++ apps/teams/views/__init__.py
@@ -1,3 +1,3 @@
-from .invitation_views import *
-from .manage_team_views import *
-from .membership_views import *
+from .invitation_views import *  # noqa F401
+from .manage_team_views import *  # noqa F401
+from .membership_views import *  # noqa F401
