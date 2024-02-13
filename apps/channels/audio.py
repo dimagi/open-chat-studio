2096201f32670385916c79b5822fe3abaae30f95
Chris Smit <chris.smit@dimagi.com>
2024-02-13 08:43:10 +0200

    Add support for codecs in convert_audio and use the opus codec for turnio ogg

--- apps/channels/audio.py
+++ apps/channels/audio.py
@@ -3,13 +3,17 @@ from io import BytesIO
 from pydub import AudioSegment
 
 
-def convert_audio(audio: BytesIO, target_format: str, source_format="ogg") -> BytesIO:
+def convert_audio(audio: BytesIO, target_format: str, source_format="ogg", codec=None) -> BytesIO:
     audio = AudioSegment.from_file(audio, format=source_format)
     # Convert to mono
     audio = audio.set_channels(1)
 
+    convertion_kwargs = {}
+    if codec:
+        convertion_kwargs["codec"] = codec
+
     new_audio = BytesIO()
-    audio.export(new_audio, format=target_format)
+    audio.export(new_audio, format=target_format, **convertion_kwargs)
     new_audio.seek(0)
     new_audio.name = f"some_name.{target_format}"
     return new_audio
