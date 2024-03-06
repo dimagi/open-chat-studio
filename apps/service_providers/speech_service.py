4ca767147bea4b4d1e4786fa9d021fc5ab42cb6c
Chris Smit <chris.smit@dimagi.com>
2024-03-06 20:26:26 +0200

    Use a SynthesizedAudio dataclass as wrapper for the synthesized audio

--- apps/service_providers/speech_service.py
+++ apps/service_providers/speech_service.py
@@ -1,5 +1,6 @@
 import tempfile
 from contextlib import closing
+from dataclasses import dataclass
 from io import BytesIO
 from typing import ClassVar
 
@@ -8,16 +9,22 @@ import boto3
 import pydantic
 from pydub import AudioSegment
 
-from apps.channels.audio import convert_audio
 from apps.chat.exceptions import AudioSynthesizeException
 from apps.experiments.models import SyntheticVoice
+
+
+@dataclass
+class SynthesizedAudio:
+    audio: BytesIO
+    duration: float
+    format: str
 
 
 class SpeechService(pydantic.BaseModel):
     _type: ClassVar[str]
     supports_transcription: ClassVar[bool] = False
 
-    def synthesize_voice(self, text: str, synthetic_voice: SyntheticVoice) -> tuple[BytesIO, float]:
+    def synthesize_voice(self, text: str, synthetic_voice: SyntheticVoice) -> SynthesizedAudio:
         assert synthetic_voice.service == self._type
         try:
             return self._synthesize_voice(text, synthetic_voice)
@@ -27,7 +34,7 @@ class SpeechService(pydantic.BaseModel):
     def transcribe_audio(self, audio: BytesIO) -> str:
         raise NotImplementedError
 
-    def _synthesize_voice(self, text, synthetic_voice) -> tuple[BytesIO, float]:
+    def _synthesize_voice(self, text, synthetic_voice) -> SynthesizedAudio:
         raise NotImplementedError
 
 
@@ -37,7 +44,7 @@ class AWSSpeechService(SpeechService):
     aws_secret_access_key: str
     aws_region: str
 
-    def _synthesize_voice(self, text: str, synthetic_voice: SyntheticVoice) -> tuple[BytesIO, float]:
+    def _synthesize_voice(self, text: str, synthetic_voice: SyntheticVoice) -> SynthesizedAudio:
         """
         Calls AWS Polly to convert the text to speech using the synthetic_voice
         """
@@ -59,7 +66,7 @@ class AWSSpeechService(SpeechService):
         duration_seconds = len(audio_segment) / 1000  # Convert milliseconds to seconds
 
         with closing(audio_stream):
-            return BytesIO(audio_data), duration_seconds
+            return SynthesizedAudio(audio=BytesIO(audio_data), duration=duration_seconds, format="mp3")
 
 
 class AzureSpeechService(SpeechService):
@@ -68,7 +75,7 @@ class AzureSpeechService(SpeechService):
     azure_subscription_key: str
     azure_region: str
 
-    def _synthesize_voice(self, text: str, synthetic_voice: SyntheticVoice) -> tuple[BytesIO, float]:
+    def _synthesize_voice(self, text: str, synthetic_voice: SyntheticVoice) -> SynthesizedAudio:
         """
         Calls Azure's cognitive speech services to convert the text to speech using the synthetic_voice
         """
@@ -97,9 +104,7 @@ class AzureSpeechService(SpeechService):
                 with open(temp_file.name, "rb") as f:
                     file_content = f.read()
 
-                return convert_audio(
-                    audio=BytesIO(file_content), target_format="mp3", source_format="wav"
-                ), duration_seconds
+                return SynthesizedAudio(audio=BytesIO(file_content), duration=duration_seconds, format="wav")
             elif result.reason == speechsdk.ResultReason.Canceled:
                 cancellation_details = result.cancellation_details
                 msg = f"Azure speech synthesis failed: {cancellation_details.reason.name}"
