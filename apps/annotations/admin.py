ac8f5be8c961a9c88c5f55fea984fa0f023d3111
Chris Smit <chris.smit@dimagi.com>
2024-03-11 17:49:03 +0200

    Add admin views for annotations

--- apps/annotations/admin.py
+++ apps/annotations/admin.py
@@ -1,1 +1,15 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import CustomTaggedItem, Tag
+
+
+@admin.register(Tag)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("name", "team", "created_at", "updated_at")
+    search_fields = ("name",)
+
+
+@admin.register(CustomTaggedItem)
+class CustomTaggedItemAdmin(admin.ModelAdmin):
+    list_display = ("user", "team")
+    search_fields = ("user__name",)
