c40d29e5eb93f60f34d9293bb5bb50a1f2b16b76
Chris Smit <chris.smit@dimagi.com>
2024-03-18 08:39:28 +0200

    Split JS into its own file and load it in the chat session view

---
+++ static/tag_multiselect.js
@@ -0,0 +1,33 @@
+{/* <script> */}
+let linkTagUrl = document.getElementById("linkTagUrl").getAttribute("data-url");
+let unlinkTagUrl = document.getElementById("unlinkTagUrl").getAttribute("data-url");
+
+var addTag = function(name, objectInfo) {
+  return function() {
+    let postData = {swap: 'none', values: {"tag_name": arguments[0], "object_info": objectInfo}};
+    htmx.ajax('POST', linkTagUrl, postData);
+  };
+};
+
+var removeTag = function(name, objectInfo) {
+  return function() {
+    let postData = {swap: 'none', values: {"tag_name": arguments[0], "object_info": objectInfo}};
+    htmx.ajax('POST', unlinkTagUrl, postData);
+  };
+};
+var settings = {
+  maxItems: 10,
+  onItemAdd: addTag('onItemAdd'),
+  onItemRemove: removeTag('onItemRemove')
+};
+document.querySelectorAll('.tag-multiselect').forEach((el)=> {
+  let objectInfo = el.getAttribute("data-info");
+
+  new TomSelect(el, {
+    maxItems: 10,
+    onItemAdd: addTag('onItemAdd', objectInfo),
+    onItemRemove: removeTag('onItemRemove', objectInfo)
+  });
+});
+
+{/* </script> */}
