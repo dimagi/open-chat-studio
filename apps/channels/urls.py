0734b51a8bc74177a1194cb6f629d5474b0e75e5
Chris Smit <chris.smit@dimagi.com>
2024-03-27 16:10:56 +0200

    Remove the current Facebook channel url + view

--- apps/channels/urls.py
+++ apps/channels/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("telegram/<uuid:channel_external_id>", views.new_telegram_message, name="new_telegram_message"),
     path("whatsapp/incoming_message", views.new_twilio_message, name="new_twilio_message"),
     path("whatsapp/turn/<uuid:experiment_id>/incoming_message", views.new_turn_message, name="new_turn_message"),
-    path("facebook/<slug:team_slug>/incoming_message", views.new_facebook_message, name="new_facebook_message"),
 ]
