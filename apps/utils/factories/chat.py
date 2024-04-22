a8e34624938036e38453ab3d25b1d2a2bd47da64
Chris Smit <chris.smit@dimagi.com>
2024-04-22 09:13:55 +0200

    add Scheduled message factories

---
+++ apps/utils/factories/chat.py
@@ -0,0 +1,27 @@
+import factory
+
+from apps.chat.models import ScheduledMessage, ScheduledMessageConfig, TimePeriod, TriggerEvent
+from apps.utils.factories.experiment import ExperimentFactory
+
+
+class ScheduledMessageConfigFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ScheduledMessageConfig
+
+    experiment = factory.SubFactory(ExperimentFactory)
+    team = factory.LazyAttribute(lambda obj: obj.experiment.team)
+    name = factory.Faker("name")
+    trigger_event = TriggerEvent.CONVERSATION_START
+    recurring = True
+    time_period = TimePeriod.DAYS
+    frequency = 1
+    repititions = 5
+    prompt_text = "Check in with the user"
+
+
+class ScheduledMessageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ScheduledMessage
+
+    schedule = factory.SubFactory(ScheduledMessageConfigFactory)
+    team = factory.LazyAttribute(lambda obj: obj.schedule.team)
