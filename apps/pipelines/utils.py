58440d3d07f48cc55abe9c1e89b87ffc6615b0a2
proteusvacuum <proteusvacuum@gmail.com>
2024-05-14 23:08:52 -0400

    Traverse graph topologically

--- apps/pipelines/utils.py
+++ apps/pipelines/utils.py
@@ -19,14 +19,14 @@ def build_runnable_from_graph(graph: PipelineGraph) -> Runnable:
     if not graph.nodes:
         raise PipelineBuildError("There are no nodes in the graph")
 
-    first_node = graph.nodes[0]
+    first_node = graph.sorted_nodes[0]
     first_node_class = getattr(nodes, first_node.type)
     if len(graph.nodes) == 1:
         # It isn't considered a "chain" if there is only a single runnable
         runnable = RunnablePassthrough() | first_node_class.build(first_node)
     else:
         runnable = first_node_class.build(first_node)
-        for node in graph.nodes[1:]:
+        for node in graph.sorted_nodes[1:]:
             node_class = getattr(nodes, node.type)
             runnable |= node_class.build(node)
     return runnable
