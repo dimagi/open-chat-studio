d1159f76d348cb83963900f27dbeb3ba52d34720
proteusvacuum <proteusvacuum@gmail.com>
2024-05-22 10:08:07 -0400

    Organize utils

--- apps/pipelines/graph.py
+++ apps/pipelines/graph.py
@@ -1,8 +1,6 @@
-from collections import defaultdict
-from functools import cached_property
-
 import pydantic
-from toposort import toposort_flatten
+from langchain_core.runnables import RunnableSequence
+from langgraph.graph import StateGraph
 
 from apps.pipelines.exceptions import PipelineBuildError
 
@@ -30,24 +28,35 @@ class PipelineGraph(pydantic.BaseModel):
         edge_data = [Edge(**edge) for edge in obj["edges"]]
         return cls(nodes=node_data, edges=edge_data)
 
-    @cached_property
-    def sorted_nodes(self):
-        """Assume a DAG"""
-        if len(self.nodes) == 1 and not self.edges:
-            # In the case that there are no edges, the nodes are already sorted...
-            return self.nodes
+    @classmethod
+    def build_runnable_from_json(cls, obj: dict) -> RunnableSequence:
+        graph = cls.from_json(obj)
+        return graph.build_runnable()
 
-        dependency_graph = defaultdict(set)
-        for edge in self.edges:
-            dependency_graph[edge.target].add(edge.source)
+    def build_runnable(self) -> RunnableSequence:
+        from apps.pipelines.nodes import nodes
+        from apps.pipelines.nodes.base import PipelineState
 
-        toposorted_ids = toposort_flatten(dependency_graph)
-
+        if not self.nodes:
+            raise PipelineBuildError("There are no nodes in the graph")
+        state_graph = StateGraph(PipelineState)
         for node in self.nodes:
-            if node.id not in toposorted_ids:
-                raise PipelineBuildError(f"Node: {node.id} is orphaned and has no edges attached to it")
+            node_class = getattr(nodes, node.type)
+            state_graph.add_node(node.id, node_class.get_callable(node))
+        for edge in self.edges:
+            state_graph.add_edge(edge.source, edge.target)
 
-        node_ids_to_nodes = {node.id: node for node in self.nodes}
-        sorted_nodes = [node_ids_to_nodes[node_id] for node_id in toposorted_ids]
+        node_ids = {n.id for n in self.nodes}
+        incoming = {e.source for e in self.edges}
+        outgoing = {e.target for e in self.edges}
+        start = list(node_ids - outgoing)
+        end = list(node_ids - incoming)
+        if len(start) != 1:
+            raise PipelineBuildError(f"Expected 1 start node, got {len(start)}")
+        if len(end) != 1:
+            raise PipelineBuildError(f"Expected 1 end node, got {len(start)}")
+        state_graph.set_entry_point(start[0])
+        state_graph.set_finish_point(end[0])
 
-        return sorted_nodes
+        compiled_graph = state_graph.compile()
+        return compiled_graph
