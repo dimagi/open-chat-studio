6759cc5f5e72148df088792e65310dcadb4b689b
Chris Smit <chris.smit@dimagi.com>
2024-05-22 14:46:49 +0200

    Add more tests

--- apps/chat/tests/test_tools.py
+++ apps/chat/tests/test_tools.py
@@ -2,8 +2,13 @@ from datetime import datetime
 
 import pytest
 import pytz
+from freezegun import freeze_time
 
-from apps.chat.agent.tools import _move_datetime_to_new_weekday_and_time
+from apps.chat.agent.schemas import WeekdaysEnum
+from apps.chat.agent.tools import UpdateScheduledMessageTool, _move_datetime_to_new_weekday_and_time
+from apps.events.models import ScheduledMessage
+from apps.utils.factories.events import EventActionFactory
+from apps.utils.factories.experiment import ExperimentSessionFactory
 
 
 @pytest.mark.parametrize(
@@ -20,9 +25,41 @@ from apps.chat.agent.tools import _move_datetime_to_new_weekday_and_time
 def test_move_datetime_to_new_weekday_and_time(
     initial_datetime_str, new_weekday, new_hour, new_minute, expected_new_datetime_str
 ):
+    """Test weekday and time changes. A weekday change will cause the datetime to jump to a different week"""
     initial_datetime = datetime.strptime(initial_datetime_str, "%Y-%m-%d %H:%M:%S")
     initial_datetime = initial_datetime.astimezone(pytz.UTC)
     new_datetime = _move_datetime_to_new_weekday_and_time(
         initial_datetime, new_weekday=new_weekday, new_hour=new_hour, new_minute=new_minute
     )
     assert new_datetime.strftime("%Y-%m-%d %H:%M:%S") == expected_new_datetime_str
+
+
+@pytest.mark.django_db()
+def test_update_schedule_tool():
+    session = ExperimentSessionFactory()
+    with freeze_time("2024-01-01"):
+        params = {"time_period": "days", "frequency": 1, "repetitions": 2, "prompt_text": "", "name": "A test schedule"}
+        message = ScheduledMessage.objects.create(
+            participant=session.participant, team=session.team, action=EventActionFactory(params=params)
+        )
+
+        expected_date = message.next_trigger_date.strftime("%A, %d %B %Y %H:%M:%S %Z")
+        assert expected_date == "Tuesday, 02 January 2024 00:00:00 UTC"
+
+        tool = UpdateScheduledMessageTool(experiment_session=session)
+        response = tool.action(
+            name="A test schedule", weekday=WeekdaysEnum.FRIDAY, hour=8, minute=0, user_specified_custom_date=False
+        )
+        message.refresh_from_db()
+        expected_date = message.next_trigger_date.strftime("%A, %d %B %Y %H:%M:%S %Z")
+        assert expected_date == "Friday, 05 January 2024 08:00:00 UTC"
+        assert response == f"The new datetime is {expected_date}"
+
+
+@pytest.mark.django_db()
+def test_user_cannot_set_custom_date():
+    tool = UpdateScheduledMessageTool(experiment_session=ExperimentSessionFactory())
+    response = tool.action(
+        name="A test schedule", weekday=WeekdaysEnum.MONDAY, hour=8, minute=0, user_specified_custom_date=True
+    )
+    assert response == "The user cannot do that. Only weekdays and time of day can be changed"
