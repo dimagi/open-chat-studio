2a7ca0ec8debbe0ae01946da63e639280e945222
proteusvacuum <proteusvacuum@gmail.com>
2024-05-23 10:54:07 -0400

    WIP Add logging with loguru
    Something weird is happening with transactions causing the DB to hang when
    saving the model

--- apps/pipelines/logging.py
+++ apps/pipelines/logging.py
@@ -1,31 +1,80 @@
 from langchain_core.callbacks import BaseCallbackHandler
+from loguru import logger
+from sentry_sdk.integrations.logging import ignore_logger
+
+from apps.pipelines.models import LogEntry, Pipeline, PipelineRunStatus
 
 
 class PipelineLoggingCallbackHandler(BaseCallbackHandler):
-    def __init__(self, pipeline) -> None:
+    def __init__(self, pipeline: Pipeline, pipeline_run, logger) -> None:
+        # logging handler
+        # self.logging_handler = logging_handler
+        # does langgraph give us a good way of getting the current step?
+
         self.pipeline = pipeline
-        self.log = ""
+        self.pipeline_run = pipeline_run
         self.depth = 0
-        self.errored = False
+        self.logger = logger
 
     def on_chain_start(self, serialized, inputs, *args, **kwargs):
         indent = "  " * self.depth
-        name = serialized.get("name", serialized.get("id", ["<unknown>"])[-1])
-        self.log = f"{self.log}\n{indent}{name} inputs: {inputs}"
+        node_id = kwargs["name"]
+        # node_id__graph:step:N__uuid
+        handler_id = f"{node_id}__{kwargs['tags'][0]}__{kwargs['parent_run_id']}"
+        # self.logger = get_logger(handler_id, self.pipeline_run.id)
+        # self.logger.info("testing")
+        # self.log = f"{self.log}\n{indent}{name} inputs: {inputs}"
         self.depth += 1
         return super().on_chain_start(serialized, inputs, *args, **kwargs)
 
     def on_chain_end(self, outputs, **kwargs):
-        from apps.pipelines.models import PipelineRun
-
         self.depth -= 1
-        self.log = f"{self.log} --> output: {outputs}\n"
+        # self.logger.info("testing")
         if self.depth == 0:
-            PipelineRun.objects.create(
-                pipeline=self.pipeline, status="SUCCESS" if not self.errored else "FAILURE", log=self.log
-            )
-            print(self.log)
+            if not self.pipeline_run.status == PipelineRunStatus.ERROR:
+                self.pipeline_run.status = PipelineRunStatus.SUCCESS
+            # self.pipeline_run.save()
 
     def on_chain_error(self, error, *args, **kwargs):
+        self.pipeline_run.status = PipelineRunStatus.ERROR
         print(f"{self.log} --/--> error: {error}")
         self.log = f"{self.log} --/--> error: {error}"
+
+
+class LogHandler:
+    def __init__(self, pipeline_run):
+        self.pipeline_run = pipeline_run
+
+    def __call__(self, message):
+        record = message.record
+        log_entry = LogEntry(
+            time=record["time"].strftime("%Y-%m-%d %H:%M:%S.%f"),
+            level=record["level"].name,
+            message=record["message"],
+            # "file": record["file"].name,
+            # "line": record["line"]
+        )
+        self.pipeline_run.append_log(log_entry)
+        # try:
+        #     pipeline_run = PipelineRun.objects.get(id=self.pipeline_run_id)
+        #     pipeline_run.append_log(log_entry)
+        # except PipelineRun.DoesNotExist:
+        #     breakpoint()
+        # pipeline_run.save()
+
+
+def get_logger(name, pipeline_run):
+    log = logger.bind(name=name)
+    log.level("DEBUG")
+    log.remove()
+    log.add(LogHandler(pipeline_run))
+    return log
+
+
+def _create_logger(name, unique_id):
+    log_name = f"{name}_{unique_id}"
+    log = logging.getLogger(log_name)
+    log.propagate = False
+    log.setLevel(logging.DEBUG)
+    ignore_logger(log_name)
+    return log
--- apps/pipelines/models.py
+++ apps/pipelines/models.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from langchain_core.runnables import RunnableConfig
+from pydantic import BaseModel as PydanticBaseModel
 
-from apps.pipelines.logging import PipelineLoggingCallbackHandler
 from apps.teams.models import BaseTeamModel
 from apps.utils.models import BaseModel
 
@@ -18,12 +21,48 @@ class Pipeline(BaseTeamModel):
 
     def invoke(self, input):
         from apps.pipelines.graph import PipelineGraph
+        from apps.pipelines.logging import PipelineLoggingCallbackHandler, get_logger
 
         runnable = PipelineGraph.build_runnable_from_json(self.data)
-        return runnable.invoke(input, config=RunnableConfig(callbacks=[PipelineLoggingCallbackHandler(self)]))
+        pipeline_run = PipelineRun.objects.create(pipeline=self, status=PipelineRunStatus.RUNNING)
+        logger = get_logger("test", pipeline_run)
+        output = runnable.invoke(
+            input, config=RunnableConfig(callbacks=[PipelineLoggingCallbackHandler(self, pipeline_run, logger)])
+        )
+        # pipeline_run.save()
+        return output
+
+
+class PipelineRunStatus(models.TextChoices):
+    RUNNING = "running", "Running"
+    SUCCESS = "success", "Success"
+    ERROR = "error", "Error"
+
+
+class LogEntry(PydanticBaseModel):
+    time: datetime
+    level: str
+    message: str
+    # file: str
+    # line: int
+
+    class Config:
+        json_encoders = {datetime: lambda v: v.strftime("%Y-%m-%d %H:%M:%S.%f")}
 
 
 class PipelineRun(BaseModel):
     pipeline = models.ForeignKey(Pipeline, on_delete=models.CASCADE, related_name="runs")
-    status = models.CharField()
-    log = models.TextField(blank=True)
+
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=128, choices=PipelineRunStatus.choices)
+    error = models.TextField(blank=True)
+    output_summary = models.TextField(blank=True)
+    log = models.JSONField(default=dict, blank=True, encoder=DjangoJSONEncoder)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def append_log(self, log_entry: LogEntry):
+        entries = self.log.get("entries", [])
+        self.log = {"entries": [*entries, log_entry.model_dump()]}
