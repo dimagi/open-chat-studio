c5e2dc690ef171a33c80746b8ba9f1eb2994066d
Chris Smit <chris.smit@dimagi.com>
2024-05-27 11:41:36 +0200

    Merge migrations into 1

--- apps/events/migrations/0009_scheduledmessage_experiment.py
+++ apps/events/migrations/0009_scheduledmessage_experiment.py
@@ -4,6 +4,20 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+def _add_experiments(apps, schema_editor):
+    ScheduledMessage = apps.get_model("events", "ScheduledMessage")
+    
+    messages = ScheduledMessage.objects.all().annotate(source_experiment_id=models.F("action__static_trigger__experiment"))
+    for message in messages:
+        message.experiment_id = message.source_experiment_id
+    
+    ScheduledMessage.objects.bulk_update(messages, fields=["experiment"])
+
+
+def _remove_experiments(apps, schema_editor):
+    ScheduledMessage = apps.get_model("events", "ScheduledMessage")
+    ScheduledMessage.objects.all().update(experiment=None)
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -16,5 +30,11 @@ class Migration(migrations.Migration):
             model_name='scheduledmessage',
             name='experiment',
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='scheduled_messages', to='experiments.experiment'),
+        ),
+        migrations.RunPython(_add_experiments, _remove_experiments),
+        migrations.AlterField(
+            model_name='scheduledmessage',
+            name='experiment',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='scheduled_messages', to='experiments.experiment'),
         ),
     ]
