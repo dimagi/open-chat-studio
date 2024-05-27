565069ae38bb8adf562a8176935247e86327d777
Chris Smit <chris.smit@dimagi.com>
2024-05-27 11:39:23 +0200

    Update rest of the code to include the experiment when creating a ScheduledMessage

--- apps/events/actions.py
+++ apps/events/actions.py
@@ -58,7 +58,9 @@ class ScheduleTriggerAction(EventActionHandlerBase):
     def invoke(self, session: ExperimentSession, action) -> str:
         from apps.events.models import ScheduledMessage
 
-        ScheduledMessage.objects.create(participant=session.participant, team=session.team, action=action)
+        ScheduledMessage.objects.create(
+            experiment=session.experiment, participant=session.participant, team=session.team, action=action
+        )
         return f"A scheduled message was created for participant '{session.participant.identifier}'"
 
     def event_action_updated(self, action):
