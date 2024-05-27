083e811c64fd29a591bca64c461bbb0a0d70e429
proteusvacuum <proteusvacuum@gmail.com>
2024-05-27 14:34:23 -0400

    Another failed attempt
    This doesn't work, but it just swallows errors

--- apps/pipelines/logging.py
+++ apps/pipelines/logging.py
@@ -1,28 +1,36 @@
+import uuid
+
+from django.db import transaction
 from langchain_core.callbacks import BaseCallbackHandler
 from loguru import logger
 
 
 class PipelineLoggingCallbackHandler(BaseCallbackHandler):
-    def __init__(self, pipeline) -> None:
+    """Handles logging within the pipeline
+
+    Since Langchain / Langgraph runs in separate threads, we append to a list
+    in a thread-safe manner, then periodically write to the DB later.
+
+    """
+
+    def __init__(self, pipeline, verbose=False) -> None:
         from apps.pipelines.models import PipelineRun
 
         self.pipeline = pipeline
         self.pipeline_run = PipelineRun.objects.create(pipeline=self.pipeline, status="SUCCESS")
-        self.logger = get_logger("foo", self.pipeline_run)
+        self.logger = get_logger(uuid.uuid4().hex, self.pipeline_run)
         self.log = ""
         self.depth = 0
         self.errored = False
 
     def on_chain_start(self, serialized, inputs, *args, **kwargs):
         self.depth += 1
-        self.logger.info("chain starting")
-        # apps.pipelines.models.PipelineRun.DoesNotExist: PipelineRun matching query does not exist.
+        self.logger.info(f"{kwargs.get('name', serialized.get('name'))} starting")
         super().on_chain_start(serialized, inputs, *args, **kwargs)
 
     def on_chain_end(self, outputs, **kwargs):
         self.depth -= 1
-        self.logger.info(outputs)
-        # apps.pipelines.models.PipelineRun.DoesNotExist: PipelineRun matching query does not exist.
+        self.logger.info(f"chain ending {outputs}")
         if self.depth == 0:
             self.logger.info(outputs)
 
@@ -31,8 +39,17 @@ class PipelineLoggingCallbackHandler(BaseCallbackHandler):
 
 
 class LogHandler:
-    def __init__(self, pipeline_run):
-        self.pipeline_run = pipeline_run
+    def __init__(self, pipeline_run_id):
+        self.pipeline_run_id = pipeline_run_id
+        self.logs = {"entries": []}
+
+    def write_log(self, entries):
+        # Write the logs in a separate transaction in a thread safe way
+        from apps.pipelines.models import PipelineRun
+
+        with transaction.atomic():
+            PipelineRun.objects.filter(pk=self.pipeline_run_id).update(log=entries)
+            # This just swallows the errors - if filter doesn't match anything, this will just do nothing
 
     def __call__(self, message):
         from apps.pipelines.models import LogEntry
@@ -45,15 +62,13 @@ class LogHandler:
                 "message": record["message"],
             }
         )
-        self.pipeline_run.refresh_from_db()
-        entries = self.pipeline_run.log.get("entries", [])
-        self.pipeline_run.log = {"entries": entries + [log_entry.model_dump()]}
-        self.pipeline_run.save()
+        self.logs["entries"].append(log_entry.model_dump())
+        self.write_log(self.logs)
 
 
 def get_logger(name, pipeline_run):
     log = logger.bind(name=name)
     log.level("DEBUG")
     log.remove()
-    log.add(LogHandler(pipeline_run))
+    log.add(LogHandler(pipeline_run.pk))
     return log
