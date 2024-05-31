ad084a0c7f8059ed50c30551bcbb077bd1ba109a
Chris Smit <chris.smit@dimagi.com>
2024-05-31 15:27:37 +0200

    ParticipantData inline for Participant model

--- apps/experiments/admin.py
+++ apps/experiments/admin.py
@@ -36,12 +36,17 @@ class SafetyLayerAdmin(admin.ModelAdmin):
     list_filter = ("team",)
 
 
+class ParticipantDataInline(admin.TabularInline):
+    model = models.ParticipantData
+
+
 @admin.register(models.Participant)
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ("identifier", "team", "public_id")
     readonly_fields = ("public_id",)
     list_filter = ("team",)
     search_fields = ("external_chat_id",)
+    inlines = [ParticipantDataInline]
 
 
 @admin.register(models.ParticipantData)
