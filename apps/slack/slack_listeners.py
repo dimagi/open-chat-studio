24d7b03c5e2cdb6b0584507ba4c29012573ad8ba
Simon Kelly <skelly@dimagi.com>
2024-06-11 17:27:50 +0200

    move installation and team load to middleware

--- apps/slack/slack_listeners.py
+++ apps/slack/slack_listeners.py
@@ -10,7 +10,7 @@ Manage event subscriptions at:
 import logging
 import re
 
-from slack_bolt import BoltContext
+from slack_bolt import BoltContext, BoltResponse
 
 from apps.channels.datamodels import SlackMessage
 from apps.channels.models import ChannelPlatform, ExperimentChannel
@@ -25,31 +25,28 @@ logger = logging.getLogger("slack.events")
 def register_listeners():
     """Register these after DB setup is complete to avoid hitting the
     wrong DB e.g. during tests"""
+    app.use(load_installation_and_team)
     app.event({"type": "message"})(new_message)
 
 
 def new_message(event, context: BoltContext):
-    slack_install = get_installation_for_context(context)
-    if not slack_install:
-        return
-
     thread_ts = event.get("thread_ts", None)
-    is_bot_mention = slack_install.bot_user_id in event.get("text", "")
+    is_bot_mention = context.bot_user_id in event.get("text", "")
 
     if is_bot_mention:
-        respond_to_message(event, context, slack_install)
+        respond_to_message(event, context)
 
-    if thread_ts and (session := get_session_for_thread(slack_install.team, thread_ts)):
-        respond_to_message(event, context, slack_install, session)
+    if thread_ts and (session := get_session_for_thread(context["team"], thread_ts)):
+        respond_to_message(event, context, session)
 
 
-def respond_to_message(event, context: BoltContext, slack_install, session=None):
+def respond_to_message(event, context: BoltContext, session=None):
     context.ack("...")
 
     channel_id = event.get("channel")
     thread_ts = event.get("thread_ts", None) or event["ts"]
     experiment_channel = ExperimentChannel.objects.filter_extras(
-        slack_install.team.slug, ChannelPlatform.SLACK, "slack_channel_id", channel_id
+        context["team"].slug, ChannelPlatform.SLACK, "slack_channel_id", channel_id
     ).first()
     if not experiment_channel:
         context.say("There are no bots associated with this channel.", thread_ts=thread_ts)
@@ -63,7 +60,7 @@ def respond_to_message(event, context: BoltContext, slack_install, session=None)
         )
 
     # strip out the mention
-    message_text = re.sub(rf"<@?{slack_install.bot_user_id}>", "", event["text"])
+    message_text = re.sub(rf"<@?{context.bot_user_id}>", "", event["text"])
     message = SlackMessage(thread_ts=thread_ts, message_text=message_text)
 
     ocs_channel = SlackChannel(experiment_channel, session, send_response_to_user=False)
@@ -71,15 +68,20 @@ def respond_to_message(event, context: BoltContext, slack_install, session=None)
     context.say(response, thread_ts=thread_ts)
 
 
-def get_installation_for_context(context: BoltContext):
+def load_installation_and_team(context: BoltContext, next):
+    """Middleware to handle loading of team etc."""
     try:
-        return SlackInstallation.objects.get(
+        installation = SlackInstallation.objects.get(
             enterprise_id=context.enterprise_id,
             slack_team_id=context.team_id,
         )
     except SlackInstallation.DoesNotExist:
         logger.error("Event received from unknown Slack installation: team_id=%s", context.team_id)
-        return
+        return BoltResponse(status=200, body="")
+
+    context["slack_install"] = installation
+    context["team"] = installation.team
+    next()
 
 
 def get_session_for_thread(team, thread_ts: str):
