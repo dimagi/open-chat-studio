db73270531dbe353b463be98f3b95440458e9ba7
Simon Kelly <skelly@dimagi.com>
2024-06-11 17:10:42 +0200

    respond to thread messages

--- apps/slack/slack_listeners.py
+++ apps/slack/slack_listeners.py
@@ -25,31 +25,29 @@ logger = logging.getLogger("slack.events")
 def register_listeners():
     """Register these after DB setup is complete to avoid hitting the
     wrong DB e.g. during tests"""
-    app.event({"type": "app_mention"})(app_mentioned)
+    app.event({"type": "message"})(new_message)
 
 
-def get_installation_for_context(context):
-    try:
-        return SlackInstallation.objects.get(
-            enterprise_id=context.enterprise_id,
-            slack_team_id=context.team_id,
-        )
-    except SlackInstallation.DoesNotExist:
-        logger.error("Event received from unknown Slack installation: team_id=%s", context.team_id)
+def new_message(event, context: BoltContext):
+    slack_install = get_installation_for_context(context)
+    if not slack_install:
         return
 
+    thread_ts = event.get("thread_ts", None)
+    is_bot_mention = slack_install.bot_user_id in event.get("text", "")
 
-def app_mentioned(slack_install, event, context: BoltContext):
-    if not slack_install:
-        slack_install = get_installation_for_context(context)
-        if not slack_install:
-            return
+    if is_bot_mention:
+        respond_to_message(event, context, slack_install)
 
-    context.ack()
+    if thread_ts and (session := get_session_for_thread(slack_install.team, thread_ts)):
+        respond_to_message(event, context, slack_install, session)
+
+
+def respond_to_message(event, context: BoltContext, slack_install, session=None):
+    context.ack("...")
 
     channel_id = event.get("channel")
     thread_ts = event.get("thread_ts", None) or event["ts"]
-
     experiment_channel = ExperimentChannel.objects.filter_extras(
         slack_install.team.slug, ChannelPlatform.SLACK, "slack_channel_id", channel_id
     ).first()
@@ -59,9 +57,7 @@ def app_mentioned(slack_install, event, context: BoltContext):
 
     slack_user = event.get("user")
 
-    try:
-        session = ExperimentSession.objects.get(team=slack_install.team, external_id=thread_ts)
-    except ExperimentSession.DoesNotExist:
+    if not session:
         session = SlackChannel.start_new_session(
             experiment_channel.experiment, experiment_channel, slack_user, slack_thread_ts=thread_ts
         )
@@ -73,3 +69,21 @@ def app_mentioned(slack_install, event, context: BoltContext):
     ocs_channel = SlackChannel(experiment_channel, session, send_response_to_user=False)
     response = ocs_channel.new_user_message(message)
     context.say(response, thread_ts=thread_ts)
+
+
+def get_installation_for_context(context: BoltContext):
+    try:
+        return SlackInstallation.objects.get(
+            enterprise_id=context.enterprise_id,
+            slack_team_id=context.team_id,
+        )
+    except SlackInstallation.DoesNotExist:
+        logger.error("Event received from unknown Slack installation: team_id=%s", context.team_id)
+        return
+
+
+def get_session_for_thread(team, thread_ts: str):
+    try:
+        return ExperimentSession.objects.get(team=team, external_id=thread_ts)
+    except ExperimentSession.DoesNotExist:
+        pass
