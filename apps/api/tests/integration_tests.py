011ff283853859b473ba4ece39f061ae8378dca5
Simon Kelly <skelly@dimagi.com>
2024-07-04 16:06:18 +0200

    add integration test

---
+++ apps/api/tests/integration_tests.py
@@ -0,0 +1,42 @@
+from unittest.mock import patch
+
+import pytest
+from django.urls import reverse
+
+from apps.utils.factories.experiment import ExperimentFactory
+from apps.utils.factories.team import TeamWithUsersFactory
+from apps.utils.tests.clients import ApiTestClient
+
+
+@pytest.fixture()
+def experiment():
+    return ExperimentFactory(team=TeamWithUsersFactory())
+
+
+@pytest.mark.django_db()
+@patch("apps.chat.channels.ApiChannel._get_experiment_response")
+def test_create_new_session_and_post_message(mock_response, experiment):
+    user = experiment.team.members.first()
+
+    client = ApiTestClient(user, experiment.team)
+    response = client.get(reverse("api:experiment-list"))
+    assert response.status_code == 200
+
+    experiment_id = response.json()["results"][0]["experiment_id"]
+
+    data = {
+        "experiment": experiment_id,
+        "messages": [
+            {"type": "ai", "message": "hi"},
+            {"type": "human", "message": "hello"},
+        ],
+    }
+    response = client.post(reverse("api:session-list"), data=data, format="json")
+    assert response.status_code == 201
+    session_id = response.json()["session_id"]
+
+    mock_response.return_value = "Fido"
+    new_message_url = reverse("channels:new_api_message", kwargs={"experiment_id": experiment_id})
+    response = client.post(new_message_url, data={"message": "What should I call my dog?", "session": session_id})
+    assert response.status_code == 200
+    assert response.json() == {"response": "Fido"}
