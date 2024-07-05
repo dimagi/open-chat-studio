bd673f504a86fa0640e3fa8a585b98586dd5a6aa
Simon Kelly <skelly@dimagi.com>
2024-07-05 10:54:24 +0200

    remove `is_admin`

--- apps/teams/roles.py
+++ apps/teams/roles.py
@@ -17,12 +17,3 @@ def is_member(user: CustomUser, team: apps.teams.models.Team) -> bool:
     if not team:
         return False
     return team.members.filter(id=user.id).exists()
-
-
-def is_admin(user: CustomUser, team: apps.teams.models.Team) -> bool:
-    if not team:
-        return False
-
-    from .models import Membership
-
-    return Membership.objects.filter(team=team, user=user, role=ROLE_ADMIN).exists()
