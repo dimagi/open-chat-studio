952fc0f56524d032aa4922a98231535be498395c
Simon Kelly <skelly@dimagi.com>
2024-07-05 11:54:06 +0200

    remove `Team.role` and `Invitation.role`

--- apps/teams/roles.py
+++ apps/teams/roles.py
@@ -1,16 +1,5 @@
-from __future__ import annotations
-
 import apps.teams.models
 from apps.users.models import CustomUser
-
-ROLE_ADMIN = "admin"
-ROLE_MEMBER = "member"
-
-ROLE_CHOICES = (
-    # customize roles here
-    (ROLE_ADMIN, "Administrator"),
-    (ROLE_MEMBER, "Member"),
-)
 
 
 def is_member(user: CustomUser, team: apps.teams.models.Team) -> bool:
