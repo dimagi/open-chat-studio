60b0e68842636b1d29365c2a7ab12844ff9b1091
Simon Kelly <skelly@dimagi.com>
2024-07-19 09:39:00 +0200

    improve api-key admin

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -6,4 +6,15 @@ from .models import UserAPIKey
 
 @admin.register(UserAPIKey)
 class UserAPIKeyModelAdmin(APIKeyModelAdmin):
-    list_display = [*APIKeyModelAdmin.list_display, "user"]
+    list_display = [
+        "prefix",
+        "team",
+        "user",
+        "name",
+        "created",
+        "expiry_date",
+        "_has_expired",
+        "revoked",
+    ]
+    list_filter = ["created", "revoked"]
+    search_fields = ["name", "prefix", "team__name", "user__username"]
