6db750464236909b92ab54755d9ffa8edd1efbc5
Simon Kelly <skelly@dimagi.com>
2024-07-22 16:29:24 +0200

    add object_pk to audit admin list view

--- apps/audit/admin.py
+++ apps/audit/admin.py
@@ -4,7 +4,15 @@ from field_audit.models import AuditEvent
 
 @admin.register(AuditEvent)
 class AuditEventAdmin(admin.ModelAdmin):
-    list_display = ["event_date", "object_class_path", "username", "transaction_id", "is_create", "is_delete"]
+    list_display = [
+        "event_date",
+        "object_class_path",
+        "object_pk",
+        "username",
+        "transaction_id",
+        "is_create",
+        "is_delete",
+    ]
     list_filter = ["event_date", "object_class_path", "is_create", "is_delete"]
     date_hierarchy = "event_date"
     ordering = ["-event_date"]
