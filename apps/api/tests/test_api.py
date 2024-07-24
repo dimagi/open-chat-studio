37317bbe4bc795a3b7f56a22dfc26500688c5a09
Simon Kelly <skelly@dimagi.com>
2024-07-24 17:14:20 +0200

    schedule update test

--- apps/api/tests/test_api.py
+++ apps/api/tests/test_api.py
@@ -1,7 +1,9 @@
 import json
 
 import pytest
+from dateutil.relativedelta import relativedelta
 from django.urls import reverse
+from django.utils import timezone
 
 from apps.experiments.models import Participant
 from apps.teams.backends import EXPERIMENT_ADMIN_GROUP, add_user_to_team
@@ -132,3 +134,43 @@ def test_update_participant_data_returns_404():
     assert response.json() == {"errors": [{"message": f"Experiment {experiment2.public_id} not found"}]}
     # Assert that nothing was created
     assert experiment.participant_data.filter(participant=participant).exists() is False
+
+
+@pytest.mark.django_db()
+def test_create_participant_schedules(experiment):
+    identifier = "part1"
+    user = experiment.team.members.first()
+    client = ApiTestClient(user, experiment.team)
+
+    # This call should create ParticipantData
+    trigger_date1 = timezone.now() + relativedelta(days=1)
+    trigger_date2 = timezone.now() + relativedelta(hours=1)
+    data = {
+        "identifier": identifier,
+        "platform": "api",
+        "data": [
+            {
+                "experiment": str(experiment.public_id),
+                "data": {"name": "John"},
+                "schedules": [
+                    {"name": "schedule1", "prompt": "tell ET to phone home", "date": trigger_date1.isoformat()},
+                    {"name": "schedule2", "prompt": "email john", "date": trigger_date2.isoformat()},
+                ],
+            },
+        ],
+    }
+    url = reverse("api:update-participant-data")
+    response = client.post(url, json.dumps(data), content_type="application/json")
+    assert response.status_code == 200
+
+    participant_data_exp_1 = experiment.participant_data.get(participant__identifier=identifier)
+    assert participant_data_exp_1.data["name"] == "John"
+    schedules = list(experiment.scheduled_messages.filter(participant__identifier=identifier))
+    assert len(schedules) == 2
+    assert schedules[0].name == "schedule1"
+    assert schedules[0].prompt == "tell ET to phone home"
+    assert schedules[0].date == trigger_date1
+
+    assert schedules[1].name == "schedule2"
+    assert schedules[1].prompt == "email john"
+    assert schedules[1].date == trigger_date2
