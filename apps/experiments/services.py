2bc10269cee7cb1ce824b13532f199ea59236e23
Steph Herbers <stephherbers@gmail.com>
2024-07-31 14:28:06 -0400

    save_experiment_version mods

--- apps/experiments/services.py
+++ apps/experiments/services.py
@@ -3,15 +3,21 @@ from experiments.models import Experiment, ExperimentVersion
 from experiments.serializers import populate_experiment_version_data
 
 
+@transaction.atomic()
 def save_experiment_version(experiment: Experiment, is_default: bool = False) -> str:
     version_data = populate_experiment_version_data(experiment)
     if version_data:
-        last_version = ExperimentVersion.objects.filter(experiment=experiment).order_by("-version_number").first()
+        last_version = (
+            ExperimentVersion.objects.filter(experiment=experiment)
+            .order_by("-version_number")
+            .values_list("version_number", flat=True)
+            .first()
+        )
         next_version_number = last_version.version_number + 1 if last_version else 1
 
         ExperimentVersion.objects.create(
             experiment=experiment,
-            version_data=version_data.json(),
+            version_data=version_data.model_dump(exclude_unset=True, exclude_none=True),
             is_default=is_default,
             version_number=next_version_number,
         )
