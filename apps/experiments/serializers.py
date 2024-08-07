c459b21007f4c7bc44bad797b71a77083868b36c
Steph Herbers <stephherbers@gmail.com>
2024-08-07 16:27:11 -0400

    fixes to serializers.py

--- apps/experiments/serializers.py
+++ apps/experiments/serializers.py
@@ -1,7 +1,6 @@
-from uuid import UUID
+from pydantic import BaseModel, Field
 
-from experiments.models import Experiment, SourceMaterial
-from pydantic import BaseModel, Field, HttpUrl
+from .models import Experiment, SourceMaterial
 
 
 class SafetyLayerData(BaseModel):
@@ -14,7 +13,7 @@ class SafetyLayerData(BaseModel):
 
 class SurveyData(BaseModel):
     name: str
-    url: HttpUrl
+    url: str
     confirmation_text: str
 
 
@@ -52,7 +51,7 @@ class ExperimentVersionData(BaseModel):
     seed_message: str | None = ""
     pre_survey: SurveyData | None
     post_survey: SurveyData | None
-    public_id: UUID
+    public_id: str
     consent_form: ConsentFormData
     synthetic_voice: SyntheticVoiceData | None
     conversational_consent_enabled: bool
@@ -62,28 +61,83 @@ class ExperimentVersionData(BaseModel):
     file_ids: list[int] = []
     tool_list: list[str] = []
 
+    class Config:
+        arbitrary_types_allowed = True
+
 
 def populate_experiment_version_data(experiment: Experiment) -> ExperimentVersionData | None:
     try:
-        return ExperimentVersionData(
+        safety_layers = [
+            SafetyLayerData(
+                name=layer.name,
+                prompt_text=layer.prompt_text,
+                messages_to_review=layer.messages_to_review,
+                default_response_to_user=layer.default_response_to_user,
+                prompt_to_bot=layer.prompt_to_bot,
+            )
+            for layer in experiment.safety_layers.all()
+        ]
+        pre_survey = (
+            SurveyData(
+                name=experiment.pre_survey.name,
+                url=experiment.pre_survey.url,
+                confirmation_text=experiment.pre_survey.confirmation_text,
+            )
+            if experiment.pre_survey
+            else None
+        )
+
+        post_survey = (
+            SurveyData(
+                name=experiment.post_survey.name,
+                url=experiment.post_survey.url,
+                confirmation_text=experiment.post_survey.confirmation_text,
+            )
+            if experiment.post_survey
+            else None
+        )
+
+        consent_form = ConsentFormData(
+            name=experiment.consent_form.name,
+            consent_text=experiment.consent_form.consent_text,
+            capture_identifier=experiment.consent_form.capture_identifier,
+            identifier_label=experiment.consent_form.identifier_label,
+            identifier_type=experiment.consent_form.identifier_type,
+            is_default=experiment.consent_form.is_default,
+            confirmation_text=experiment.consent_form.confirmation_text,
+        )
+
+        synthetic_voice = (
+            SyntheticVoiceData(
+                name=experiment.synthetic_voice.name,
+                neural=experiment.synthetic_voice.neural,
+                language=experiment.synthetic_voice.language,
+                language_code=experiment.synthetic_voice.language_code,
+                gender=experiment.synthetic_voice.gender,
+                service=experiment.synthetic_voice.service,
+                voice_provider=experiment.synthetic_voice.voice_provider,
+                file=experiment.synthetic_voice.file,
+            )
+            if experiment.synthetic_voice
+            else None
+        )
+
+        version_data = ExperimentVersionData(
             name=experiment.name,
             description=experiment.description,
-            llm_provider_id=experiment.llm_provider.id if experiment.llm_provider else None,
-            llm=experiment.llm,
             assistant_id=experiment.assistant.id if experiment.assistant else None,
             temperature=experiment.temperature,
             prompt_text=experiment.prompt_text,
             input_formatter=experiment.input_formatter,
-            safety_layer_ids=[layer.id for layer in experiment.safety_layers.all()],
+            safety_layers=safety_layers,
             is_active=experiment.is_active,
-            source_material_id=experiment.source_material.id if experiment.source_material else None,
+            source_material=experiment.source_material,
             seed_message=experiment.seed_message,
-            pre_survey_id=experiment.pre_survey.id if experiment.pre_survey else None,
-            post_survey_id=experiment.post_survey.id if experiment.post_survey else None,
+            pre_survey=pre_survey,
+            post_survey=post_survey,
             public_id=experiment.public_id,
-            consent_form_id=experiment.consent_form.id,
-            voice_provider_id=experiment.voice_provider.id if experiment.voice_provider else None,
-            synthetic_voice_id=experiment.synthetic_voice.id if experiment.synthetic_voice else None,
+            consent_form=consent_form,
+            synthetic_voice=synthetic_voice,
             conversational_consent_enabled=experiment.conversational_consent_enabled,
             safety_violation_notification_emails=experiment.safety_violation_notification_emails,
             max_token_limit=experiment.max_token_limit,
@@ -91,6 +145,7 @@ def populate_experiment_version_data(experiment: Experiment) -> ExperimentVersio
             file_ids=[file.id for file in experiment.files.all()],
             tool_list=experiment.tools,
         )
+        return version_data
     except Exception as e:
         print(f"Error populating experiment version data: {e}")
         return None
