d07c628bda42a8335b037ec5ca549f6c44333c65
Steph Herbers <stephherbers@gmail.com>
2024-08-19 14:59:10 -0400

    when creating a new version, copy over eventActions and if connected to a pipeline make a copy of the pipeline and attch that copied pipeline_id to the new eventAction

--- apps/experiments/utils.py
+++ apps/experiments/utils.py
@@ -1,5 +1,8 @@
 from django.db import transaction
 
+from apps.analysis.core import Pipeline
+from apps.events.models import EventAction
+
 from .models import Experiment
 
 
@@ -18,6 +21,7 @@ def create_experiment_version(original_experiment_id):
         new_experiment.save()
         new_experiment.safety_layers.set(original_experiment.safety_layers.all())
         new_experiment.files.set(original_experiment.files.all())
+        copy_event_actions(original_experiment_id)
     return new_experiment
 
 
@@ -30,3 +34,15 @@ def calculate_version_number(parent_experiment_id):
         or 0
     )
     return last_version_number + 1
+
+
+def copy_event_actions(experiment_id):
+    event_actions = EventAction.objects.filter(experiment_id=experiment_id)
+    for event_action in event_actions:
+        event_action.pk = None
+        if "pipeline_id" in event_action.params:
+            old_pipeline_id = event_action.params["pipeline_id"]
+            old_pipeline = Pipeline.objects.get(id=old_pipeline_id)
+            new_pipeline = Pipeline.objects.create(name=f"{old_pipeline.name} (copy)", data=old_pipeline.data)
+            event_action.params["pipeline_id"] = new_pipeline.id
+        event_action.save()
