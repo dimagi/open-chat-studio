33246322df923e8a6b93cfde189e535d34461dae
Simon Kelly <skelly@dimagi.com>
2024-10-04 12:20:42 +0200

    update markdown renderer

--- apps/utils/markdown.py
+++ apps/utils/markdown.py
@@ -33,9 +33,20 @@ class FileLinkInlineProcessor(LinkInlineProcessor):
         return href, title, index, handled
 
 
+class FileImageInlineProcessor(ImageInlineProcessor):
+    def getLink(self, data, index):
+        href, title, index, handled = super().getLink(data, index)
+        if href.startswith("file"):
+            _, team_slug, session_id, file_id = href.split(":")
+            relative_url = reverse("experiments:download_file", args=[team_slug, session_id, file_id])
+            href = relative_url
+        return href, title, index, handled
+
+
 class FileExtension(markdown.Extension):
     def extendMarkdown(self, md, *args, **kwargs):
         md.inlinePatterns.register(FileLinkInlineProcessor(LINK_RE, md), "link", 160)
+        md.inlinePatterns.register(FileImageInlineProcessor(IMAGE_LINK_RE, md), "image_link", 150)
 
 
 class ResourceExtension(markdown.Extension):
