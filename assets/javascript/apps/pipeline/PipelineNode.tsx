194369f14f0861220250cd51245583486e154901
Simon Kelly <skelly@dimagi.com>
2024-10-22 15:32:47 +0200

    extract getInputWidget and simplify

--- assets/javascript/apps/pipeline/PipelineNode.tsx
+++ assets/javascript/apps/pipeline/PipelineNode.tsx
@@ -43,7 +43,6 @@ const getCachedData: () => typeof localCache = () => {
 
 export function PipelineNode({ id, data, selected }: NodeProps<NodeData>) {
   const cachedData = getCachedData();
-  const parameterValues: NodeParameterValues = cachedData.parameterValues;
   const defaultValues = cachedData.defaultValues;
   const setNode = usePipelineStore((state) => state.setNode);
   const deleteNode = usePipelineStore((state) => state.deleteNode);
@@ -57,9 +56,7 @@ export function PipelineNode({ id, data, selected }: NodeProps<NodeData>) {
   const [params, setParams] = useState(data.params || defaultParams);
 
   const updateParamValue = (
-    event: ChangeEvent<
-      HTMLTextAreaElement | HTMLSelectElement | HTMLInputElement
-    >,
+    event: ChangeEvent<HTMLTextAreaElement | HTMLSelectElement | HTMLInputElement>,
   ) => {
     const { name, value } = event.target;
     setParams((prevParams) => {
@@ -78,178 +75,38 @@ export function PipelineNode({ id, data, selected }: NodeProps<NodeData>) {
     });
   };
 
-  const getInputWidget = (inputParam: InputParam) => {
-    switch (inputParam.type) {
-      case "LlmTemperature":
-        return (
-          <>
-            <div className="m-1 font-medium text-center">Temperature</div>
-            <input
-              className="input input-bordered w-full"
-              name={inputParam.name}
-              onChange={updateParamValue}
-              value={params[inputParam.name]}
-              type="number"
-              step=".1"
-            ></input>
-          </>
-        );
-      case "LlmProviderId":
-        return (
-          <>
-            <div className="m-1 font-medium text-center">LLM Provider</div>
-            <LlmProviderIdWidget
-              parameterValues={parameterValues}
-              inputParam={inputParam}
-              value={params[inputParam.name]}
-              setParams={setParams}
-              id={id}
-            />
-          </>
-        );
-      case "SourceMaterialId":
-        return (
-          <>
-            <div className="m-1 font-medium text-center">Source Material</div>
-            <SourceMaterialIdWidget
-              parameterValues={parameterValues}
-              onChange={updateParamValue}
-              inputParam={inputParam}
-              value={params[inputParam.name]}
-            />
-          </>
-        );
-      case "LlmModel":
-        return (
-          <>
-            <div className="m-1 font-medium text-center">LLM Model</div>
-            <LlmModelWidget
-              parameterValues={parameterValues}
-              inputParam={inputParam}
-              value={params[inputParam.name]}
-              onChange={updateParamValue}
-              provider={
-                Array.isArray(params.llm_provider_id)
-                  ? params.llm_provider_id.join("")
-                  : params.llm_provider_id
-              }
-            />
-          </>
-        );
-      case "NumOutputs":
-        return (
-          <>
-            <div className="m-1 font-medium text-center">Number of Outputs</div>
-            <input
-              className="input input-bordered w-full"
-              name={inputParam.name}
-              onChange={updateParamValue}
-              value={params[inputParam.name] || 1}
-              type="number"
-              step="1"
-            ></input>
-          </>
-        );
-      case "Keywords": {
-        const length =
-          parseInt(
-            Array.isArray(params.num_outputs)
-              ? params.num_outputs.join("")
-              : params.num_outputs,
-          ) || 1;
-        return (
-          <>
-            {Array.from({ length: length }, (_, index) => {
-              return (
-                <KeywordsWidget
-                  index={index}
-                  keywords={
-                    Array.isArray(params.keywords) ? params["keywords"] : []
-                  }
-                  setParams={setParams}
-                  id={id}
-                  key={`${inputParam.name}-${index}`}
-                ></KeywordsWidget>
-              );
-            })}
-          </>
-        );
-      }
-      case "HistoryType": {
-        return (
-          <>
-            <div className="m-1 font-medium text-center">History Type</div>
-            <HistoryTypeWidget
-              onChange={updateParamValue}
-              inputParam={inputParam}
-              value={params[inputParam.name]}
-            ></HistoryTypeWidget>
-          </>
-        );
-      }
-      case "HistoryName": {
-        if (params["history_type"] !== "named") {
-          return <></>;
-        }
-        return (
-          <>
-            <div className="m-1 font-medium text-center">History Name</div>
-            <HistoryNameWidget
-              onChange={updateParamValue}
-              inputParam={inputParam}
-              value={params[inputParam.name]}
-            ></HistoryNameWidget>
-          </>
-        );
-      }
-      case "MaxTokenLimit": {
-        if (params["history_type"] !== "global") {
-          return <></>;
-        }
-        return (
-          <>
-            <div className="m-1 font-medium text-center">
-              Maximum Token Limit
-            </div>
-            <MaxTokenLimitWidget
-              onChange={updateParamValue}
-              inputParam={inputParam}
-              value={params[inputParam.name]}
-            ></MaxTokenLimitWidget>
-          </>
-        );
-      }
-      default: {
-        const humanName = inputParam.human_name
-          ? inputParam.human_name
-          : inputParam.name.replace(/_/g, " ");
-        return (
-          <>
-            <div className="m-1 font-medium text-center capitalize">
-              {humanName}
-            </div>
-            <TextWidget
-              humanName={humanName}
-              name={inputParam.name}
-              onChange={updateParamValue}
-              value={params[inputParam.name] || ""}
-            ></TextWidget>
-          </>
-        );
-      }
-    }
-  };
+  const handlesFactory = getOutputFactory(data.type);
 
   return (
-    <PipelineNodeComponent
-      id={id}
-      data={data}
-      selected={selected}
-      params={params}
-      getInputWidget={getInputWidget}
-      getOutputHandles={getOutputFactory(data.type)}
-      deleteNode={deleteNode}
-    />
+    <>
+      <NodeToolbar position={Position.Top}>
+        <div className="join">
+          <button
+            className="btn btn-xs join-item"
+            onClick={() => deleteNode(id)}
+          >
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </div>
+      </NodeToolbar>
+      <div
+        className={classNames(
+          selected ? "border border-primary" : "border",
+          "px-4 py-2 shadow-md rounded-xl border-2 border-stone-400 bg-base-100",
+        )}
+      >
+        <Handle type="target" position={Position.Left} id="input" />
+        <div className="ml-2">
+          <div className="m-1 text-lg font-bold text-center">{data.label}</div>
+          {data.inputParams.map((inputParam) => (
+            <React.Fragment key={inputParam.name}>
+              {getInputWidget({id : id, inputParam : inputParam, params : params, setParams : setParams, updateParamValue : updateParamValue})}
+            </React.Fragment>
+          ))}
+        </div>
+        {handlesFactory(params)}
+      </div>
+    </>
   );
 }
 
@@ -319,54 +176,173 @@ const booleanOutputs = () => {
   return <>{outputHandles}</>
 }
 
-interface PipelineNodeComponentProps {
+interface InputWidgetParams {
   id: string;
-  data: NodeData;
-  selected: boolean;
+  inputParam: InputParam;
   params: NodeParams;
-  getInputWidget: (inputParam: InputParam) => React.JSX.Element;
-  getOutputHandles: (params: NodeParams) => React.JSX.Element;
-  deleteNode: (id: string) => void;
+  setParams: React.Dispatch<React.SetStateAction<NodeParams>>;
+  updateParamValue: (event: React.ChangeEvent<HTMLTextAreaElement | HTMLSelectElement | HTMLInputElement>) => any;
 }
 
-const PipelineNodeComponent = ({
-  id,
-  data,
-  selected,
-  params,
-  getInputWidget,
-  getOutputHandles,
-  deleteNode,
-}: PipelineNodeComponentProps) => {
-  return (
-    <>
-      <NodeToolbar position={Position.Top}>
-        <div className="join">
-          <button
-            className="btn btn-xs join-item"
-            onClick={() => deleteNode(id)}
-          >
-            <i className="fa fa-trash-o"></i>
-          </button>
-        </div>
-      </NodeToolbar>
-      <div
-        className={classNames(
-          selected ? "border border-primary" : "border",
-          "px-4 py-2 shadow-md rounded-xl border-2 border-stone-400 bg-base-100",
-        )}
-      >
-        <Handle type="target" position={Position.Left} id="input" />
-        <div className="ml-2">
-          <div className="m-1 text-lg font-bold text-center">{data.label}</div>
-          {data.inputParams.map((inputParam) => (
-            <React.Fragment key={inputParam.name}>
-              {getInputWidget(inputParam)}
-            </React.Fragment>
-          ))}
-        </div>
-        {getOutputHandles(params)}
-      </div>
-    </>
-  );
+const getInputWidget = ({id, inputParam, params, setParams, updateParamValue}: InputWidgetParams) => {
+  const parameterValues = getCachedData().parameterValues;
+  switch (inputParam.type) {
+    case "LlmTemperature":
+      return (
+        <>
+          <div className="m-1 font-medium text-center">Temperature</div>
+          <input
+            className="input input-bordered w-full"
+            name={inputParam.name}
+            onChange={updateParamValue}
+            value={params[inputParam.name]}
+            type="number"
+            step=".1"
+          ></input>
+        </>
+      );
+    case "LlmProviderId":
+      return (
+        <>
+          <div className="m-1 font-medium text-center">LLM Provider</div>
+          <LlmProviderIdWidget
+            parameterValues={parameterValues}
+            inputParam={inputParam}
+            value={params[inputParam.name]}
+            setParams={setParams}
+            id={id}
+          />
+        </>
+      );
+    case "SourceMaterialId":
+      return (
+        <>
+          <div className="m-1 font-medium text-center">Source Material</div>
+          <SourceMaterialIdWidget
+            parameterValues={parameterValues}
+            onChange={updateParamValue}
+            inputParam={inputParam}
+            value={params[inputParam.name]}
+          />
+        </>
+      );
+    case "LlmModel":
+      return (
+        <>
+          <div className="m-1 font-medium text-center">LLM Model</div>
+          <LlmModelWidget
+            parameterValues={parameterValues}
+            inputParam={inputParam}
+            value={params[inputParam.name]}
+            onChange={updateParamValue}
+            provider={
+              Array.isArray(params.llm_provider_id)
+                ? params.llm_provider_id.join("")
+                : params.llm_provider_id
+            }
+          />
+        </>
+      );
+    case "NumOutputs":
+      return (
+        <>
+          <div className="m-1 font-medium text-center">Number of Outputs</div>
+          <input
+            className="input input-bordered w-full"
+            name={inputParam.name}
+            onChange={updateParamValue}
+            value={params[inputParam.name] || 1}
+            type="number"
+            step="1"
+          ></input>
+        </>
+      );
+    case "Keywords": {
+      const length =
+        parseInt(
+          Array.isArray(params.num_outputs)
+            ? params.num_outputs.join("")
+            : params.num_outputs,
+        ) || 1;
+      return (
+        <>
+          {Array.from({ length: length }, (_, index) => {
+            return (
+              <KeywordsWidget
+                index={index}
+                keywords={
+                  Array.isArray(params.keywords) ? params["keywords"] : []
+                }
+                setParams={setParams}
+                id={id}
+                key={`${inputParam.name}-${index}`}
+              ></KeywordsWidget>
+            );
+          })}
+        </>
+      );
+    }
+    case "HistoryType": {
+      return (
+        <>
+          <div className="m-1 font-medium text-center">History Type</div>
+          <HistoryTypeWidget
+            onChange={updateParamValue}
+            inputParam={inputParam}
+            value={params[inputParam.name]}
+          ></HistoryTypeWidget>
+        </>
+      );
+    }
+    case "HistoryName": {
+      if (params["history_type"] !== "named") {
+        return <></>;
+      }
+      return (
+        <>
+          <div className="m-1 font-medium text-center">History Name</div>
+          <HistoryNameWidget
+            onChange={updateParamValue}
+            inputParam={inputParam}
+            value={params[inputParam.name]}
+          ></HistoryNameWidget>
+        </>
+      );
+    }
+    case "MaxTokenLimit": {
+      if (params["history_type"] !== "global") {
+        return <></>;
+      }
+      return (
+        <>
+          <div className="m-1 font-medium text-center">
+            Maximum Token Limit
+          </div>
+          <MaxTokenLimitWidget
+            onChange={updateParamValue}
+            inputParam={inputParam}
+            value={params[inputParam.name]}
+          ></MaxTokenLimitWidget>
+        </>
+      );
+    }
+    default: {
+      const humanName = inputParam.human_name
+        ? inputParam.human_name
+        : inputParam.name.replace(/_/g, " ");
+      return (
+        <>
+          <div className="m-1 font-medium text-center capitalize">
+            {humanName}
+          </div>
+          <TextWidget
+            humanName={humanName}
+            name={inputParam.name}
+            onChange={updateParamValue}
+            value={params[inputParam.name] || ""}
+          ></TextWidget>
+        </>
+      );
+    }
+  }
 };
