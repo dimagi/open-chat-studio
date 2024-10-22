3aee3ccda68aaa6596cfe58442d6156cee80ea8c
Simon Kelly <skelly@dimagi.com>
2024-10-22 13:07:10 +0200

    create global for `alertify`

--- assets/javascript/apps/pipeline/stores/pipelineManagerStore.ts
+++ assets/javascript/apps/pipeline/stores/pipelineManagerStore.ts
@@ -50,7 +50,6 @@ const usePipelineManagerStore = create<PipelineManagerStoreType>((set, get) => (
           if (updatedFlow) {
             set({currentPipeline: pipeline});
             resolve();
-            // @ts-expect-error. This module is available
             alertify.success(saveText).delay(2);
           }
         })
