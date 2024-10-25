180d9997ea8302dddfcbec877bdac160c6ba23a7
Simon Kelly <skelly@dimagi.com>
2024-10-25 12:38:39 +0200

    exandable editor panel

--- assets/javascript/apps/pipeline/panel/EditPanel.tsx
+++ assets/javascript/apps/pipeline/panel/EditPanel.tsx
@@ -1,7 +1,7 @@
 import React, {ChangeEvent, useEffect, useState} from "react";
 import useEditorStore from "../stores/editorStore";
 import OverlayPanel from "../components/OverlayPanel";
-import {getCachedData} from "../utils";
+import {classNames, getCachedData} from "../utils";
 import usePipelineStore from "../stores/pipelineStore";
 import {getInputWidget} from "../nodes/GetInputWidget";
 import {InputParam} from "../types/nodeInputTypes";
@@ -10,6 +10,7 @@ import {InputParam} from "../types/nodeInputTypes";
 export default function EditPanel({nodeId}: { nodeId: string }) {
   const closeEditor = useEditorStore((state) => state.closeEditor);
   const getNode = usePipelineStore((state) => state.getNode);
+  const [expanded, setExpanded] = useState(false);
 
   const {id, data} = getNode(nodeId)!;
   const cachedData = getCachedData();
@@ -46,15 +47,24 @@ export default function EditPanel({nodeId}: { nodeId: string }) {
     });
   };
 
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  }
+
+  const width = expanded ? "w-full" : "w-2/5";
+
   return (
     <div className="relative">
-      <OverlayPanel classes="top-0 right-0 w-2/5 h-[80vh] overflow-y-auto" isOpen={true}>
+      <OverlayPanel classes={classNames("top-0 right-0 h-[80vh] overflow-y-auto", width)} isOpen={true}>
         <>
+          <div className="absolute top-0 left-0">
+            <button className="btn btn-xs btn-ghost" onClick={toggleExpand}>
+              {expanded ? <i className="fa-solid fa-down-left-and-up-right-to-center"></i> :
+                <i className="fa-solid fa-up-right-and-down-left-from-center"></i>}
+            </button>
+          </div>
           <div className="absolute top-0 right-0">
-            <button
-              className="btn btn-xs btn-ghost"
-              onClick={closeEditor}
-            >
+            <button className="btn btn-xs btn-ghost" onClick={closeEditor}>
               <i className="fa fa-times"></i>
             </button>
           </div>
