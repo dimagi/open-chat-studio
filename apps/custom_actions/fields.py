fad83898dcbfd7b3896ff832b50c738ebf54a4df
Simon Kelly <skelly@dimagi.com>
2024-10-30 12:40:36 +0200

    based crud setup for custom actions

---
+++ apps/custom_actions/fields.py
@@ -0,0 +1,68 @@
+import json
+
+import yaml
+from django.core.exceptions import ValidationError
+from django.forms import Textarea
+from django.forms.fields import CharField, InvalidJSONInput, JSONString
+
+
+class JSONORYAMLField(CharField):
+    default_error_messages = {
+        "invalid": "Enter a valid JSON or YAML.",
+    }
+    widget = Textarea
+
+    def __init__(self, encoder=None, decoder=None, **kwargs):
+        self.encoder = encoder
+        self.decoder = decoder
+        super().__init__(**kwargs)
+
+    def to_python(self, value):
+        if self.disabled:
+            return value
+        if value in self.empty_values:
+            return None
+        elif isinstance(value, list | dict | int | float | JSONString):
+            return value
+        try:
+            if value.strip().startswith(("{", "[")):
+                converted = json.loads(value, cls=self.decoder)
+            else:
+                converted = yaml.safe_load(value)
+        except (json.JSONDecodeError, yaml.YAMLError):
+            raise ValidationError(
+                self.error_messages["invalid"],
+                code="invalid",
+                params={"value": value},
+            )
+        if isinstance(converted, str):
+            return JSONString(converted)
+        else:
+            return converted
+
+    def bound_data(self, data, initial):
+        if self.disabled:
+            return initial
+        if data is None:
+            return None
+        try:
+            if data.strip().startswith(("{", "[")):
+                return json.loads(data, cls=self.decoder)
+            else:
+                return yaml.safe_load(data)
+        except (json.JSONDecodeError, yaml.YAMLError):
+            return InvalidJSONInput(data)
+
+    def prepare_value(self, value):
+        if isinstance(value, InvalidJSONInput):
+            return value
+        return json.dumps(value, ensure_ascii=False, cls=self.encoder, indent=2)
+
+    def has_changed(self, initial, data):
+        if super().has_changed(initial, data):
+            return True
+        # For purposes of seeing whether something has changed, True isn't the
+        # same as 1 and the order of keys doesn't matter.
+        return json.dumps(initial, sort_keys=True, cls=self.encoder) != json.dumps(
+            self.to_python(data), sort_keys=True, cls=self.encoder
+        )
---
+++ apps/custom_actions/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.urls import reverse
+from field_audit import audit_fields
+from field_audit.models import AuditingManager
+
+from apps.teams.models import BaseTeamModel
+
+
+@audit_fields("team", "name", "prompt", "api_schema", audit_special_queryset_writes=True)
+class CustomAction(BaseTeamModel):
+    objects = AuditingManager()
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    prompt = models.TextField(blank=True)
+    api_schema = models.JSONField()
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("custom_actions:edit", args=[self.team.slug, self.pk])
