a947893c4bd2fad6cbbd1f9e6a22f544158be138
Farid Rener <proteusvacuum@gmail.com>
2024-10-31 10:09:54 -0400

    Update admin to show perninent info

--- apps/service_providers/admin.py
+++ apps/service_providers/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
+from apps.analysis.models import Analysis
+from apps.assistants.models import OpenAiAssistant
 from apps.experiments.models import Experiment
 from apps.pipelines.models import Node
 
@@ -24,15 +26,46 @@ class ExperimentInline(admin.TabularInline):
 
 @admin.register(LlmProviderModel)
 class LlmProviderModelAdmin(admin.ModelAdmin):
-    list_display = ("name", "type", "max_token_limit", "team", "related_nodes")
+    list_display = ("name", "type", "max_token_limit", "team")
     list_filter = ("team", "type", "name")
     inlines = [ExperimentInline]
+    readonly_fields = ["related_experiments", "related_assistants", "related_analyses", "related_nodes"]
+
+    def related_experiments(self, obj):
+        experiments = Experiment.objects.filter(llm_provider_model_id=str(obj.id))
+        experiment_urls = [
+            f"<a href={reverse('admin:experiments_experiment_change',args=[experiment.id])} >{str(experiment)}</a>"
+            for experiment in experiments
+        ]
+        return format_html("<br>".join(experiment_urls))
+
+    related_experiments.short_description = "Experiment Usage"
+
+    def related_assistants(self, obj):
+        assistants = OpenAiAssistant.objects.filter(llm_provider_model_id=str(obj.id))
+        assistant_urls = [
+            f"<a href={reverse('admin:assistants_openaiassistant_change',args=[assistant.id])} >{str(assistant)}</a>"
+            for assistant in assistants
+        ]
+        return format_html("<br>".join(assistant_urls))
+
+    related_assistants.short_description = "Assistant Usage"
+
+    def related_analyses(self, obj):
+        analyses = Analysis.objects.filter(llm_provider_model_id=str(obj.id))
+        analysis_urls = [
+            f"<a href={reverse('admin:analysis_analysis_change',args=[analysis.id])} >{str(analyses)}</a>"
+            for analysis in analyses
+        ]
+        return format_html("<br>".join(analysis_urls))
+
+    related_assistants.short_description = "Analysis Usage"
 
     def related_nodes(self, obj):
         nodes = Node.objects.filter(params__llm_provider_model_id=str(obj.id))
         pipelines = set(node.pipeline for node in nodes)
         pipeline_urls = [
-            f"<a href={reverse('admin:pipelines_pipeline_change',args=[pipeline.id])} >{pipeline.name}</a>"
+            f"<a href={reverse('admin:pipelines_pipeline_change',args=[pipeline.id])} >{str(pipeline)}</a>"
             for pipeline in pipelines
         ]
         return format_html("<br>".join(pipeline_urls))
