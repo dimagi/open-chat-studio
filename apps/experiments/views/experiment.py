1e494dc94ecd5ed3f14d92355ceb359b2926b787
Chris Smit <chris.smit@dimagi.com>
2024-10-31 08:21:40 +0200

    Fix: Only try to send an email if the identifier type is email

--- apps/experiments/views/experiment.py
+++ apps/experiments/views/experiment.py
@@ -925,7 +925,7 @@ def start_session_public(request, team_slug: str, experiment_id: str):
                 participant_identifier=identifier,
                 timezone=request.session.get("detected_tz", None),
             )
-            if verify_user:
+            if verify_user and consent.identifier_type == "email":
                 return _verify_user_or_start_session(
                     identifier=identifier,
                     request=request,
