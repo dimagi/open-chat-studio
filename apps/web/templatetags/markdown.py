5c6b02eb68a1343e699018eb22f126c060cb29d0
Simon Kelly <skelly@dimagi.com>
2024-11-06 16:30:24 +0200

    remove templates and template tags

--- apps/web/templatetags/markdown.py
+++ apps/web/templatetags/markdown.py
@@ -1,20 +1,7 @@
-import markdown
 from django import template
-from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
-from apps.utils.markdown import ResourceExtension
-
 register = template.Library()
-
-
-@register.filter
-@stringfilter
-def render_markdown(value):
-    if not value:
-        return ""
-    md = markdown.Markdown(extensions=["fenced_code", "tables", "footnotes", ResourceExtension()])
-    return mark_safe(md.convert(value))
 
 
 @register.filter
