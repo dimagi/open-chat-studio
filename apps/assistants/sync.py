e0a19e4c42f07684bf13ab5c08ce123b6d80fd63
Chris Smit <64970009+SmittieC@users.noreply.github.com>
2024-11-11 12:51:13 +0200

    Update apps/assistants/sync.py
    Co-authored-by: Simon Kelly <skelly@dimagi.com>

--- apps/assistants/sync.py
+++ apps/assistants/sync.py
@@ -221,10 +221,10 @@ def _get_tool_file_ids_from_openai(client, assistant_data, resource: ToolResourc
     elif resource.tool_type == "file_search":
         openai_vector_store_id = resource.extra.get("vector_store_id")
         if openai_vector_store_id:
-            [
-                openai_file_ids.append(file.id)
+            openai_file_ids.extend([
+                file.id
                 for file in client.beta.vector_stores.files.list(vector_store_id=openai_vector_store_id)
-            ]
+            ])
     return openai_file_ids
 
 
