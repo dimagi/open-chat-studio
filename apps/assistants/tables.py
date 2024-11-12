3b0cb7a6ab8ddd64849a03d6eb565ed731117543
Steph Herbers <stephherbers@gmail.com>
2024-11-12 13:38:23 -0500

    clean up OpenAiAssistantTable

--- apps/assistants/tables.py
+++ apps/assistants/tables.py
@@ -1,8 +1,19 @@
+from django.conf import settings
 from django.utils.safestring import mark_safe
 from django_tables2 import columns, tables
 
 from apps.assistants.models import OpenAiAssistant
 from apps.generics import actions
+
+
+def get_assistant_row_attrs():
+    def _get_redirect_url(record):
+        return record.get_absolute_url() if record.working_version is None else ""
+
+    return {
+        **settings.DJANGO_TABLES2_ROW_ATTRS,
+        "data-redirect-url": _get_redirect_url,
+    }
 
 
 class OpenAiAssistantTable(tables.Table):
@@ -56,22 +67,6 @@ class OpenAiAssistantTable(tables.Table):
     class Meta:
         model = OpenAiAssistant
         fields = ("name", "assistant_id")
-        row_attrs = {
-            "class": lambda record: " ".join(
-                [
-                    "border-b",
-                    "border-base-300",
-                    "hover:bg-base-200",
-                    "data-[redirect-url]:[&:not([data-redirect-url=''])]:hover:cursor-pointer",
-                    "disabled" if record.working_version is None else "",
-                ]
-            ),
-            "id": lambda record: f"record-{record.id}",
-            "data-redirect-url": lambda record: (
-                record.get_absolute_url()
-                if hasattr(record, "get_absolute_url") and record.working_version is None
-                else ""
-            ),
-        }
+        row_attrs = get_assistant_row_attrs()
         orderable = False
         empty_text = "No assistants found."
