bc66e33825a9087eaaa82bb6cbc2479b44f7192e
Simon Kelly <skelly@dimagi.com>
2024-11-27 13:32:53 +0200

    color edges after test

--- assets/javascript/apps/pipeline/AnnotatedEdge.tsx
+++ assets/javascript/apps/pipeline/AnnotatedEdge.tsx
@@ -28,14 +28,15 @@ const AnnotatedEdge: FC<EdgeProps<Edge<{ label: string }>>> = ({
 
   return (
     <>
-      <BaseEdge id={id} path={edgePath} />
+      <BaseEdge id={id} path={edgePath} style={{ stroke: "green", strokeWidth: 2, fontWeight: "bold" }} />
       <EdgeLabelRenderer>
         <div
           style={{
             transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
             pointerEvents: "all",
+            borderColor: "green"
           }}
-          className="nodrag nopan nowheel px-4 py-2 shadow-md rounded-xl border-2 bg-base-100 max-w-xs border-dotted border-success max-h-40 overflow-auto z-20"
+          className="nodrag nopan nowheel px-4 py-2 shadow-md rounded-xl border-2 bg-base-100 max-w-xs max-h-40 overflow-auto z-20"
         >
           {label}
         </div>
