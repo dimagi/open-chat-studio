7791abf497801a2b81b3a54c83d41f8124f8c73e
Simon Kelly <skelly@dimagi.com>
2024-12-03 17:49:06 +0200

    refactor mocking

--- apps/service_providers/tests/test_runnables_with_tools.py
+++ apps/service_providers/tests/test_runnables_with_tools.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from typing import Any
 from unittest.mock import Mock, patch
 
@@ -34,22 +35,15 @@ def session(fake_llm_service):
     return session
 
 
-@patch("apps.service_providers.llm_service.runnables.AssistantChat._get_output_with_annotations")
 @patch("openai.resources.beta.threads.runs.runs.Runs.submit_tool_outputs")
-@patch("openai.resources.beta.threads.messages.Messages.list")
-@patch("openai.resources.beta.threads.runs.Runs.retrieve")
-@patch("openai.resources.beta.Threads.create_and_run")
 @pytest.mark.django_db()
-def test_assistant_tool_response(
-    create_and_run, retrieve_run, list_messages, submit_tool_outputs, get_output_with_annotations, session
-):
-    run = configure_mocks(session, create_and_run, retrieve_run, list_messages, get_output_with_annotations)
-    submit_tool_outputs.return_value = run
+def test_assistant_tool_response(submit_tool_outputs, session):
+    with configure_common_mocks(session) as run:
+        submit_tool_outputs.return_value = run
+        tool = _make_tool_for_testing("test tool output")
+        runnable = get_runnable(session, tool)
+        result = runnable.invoke("test")
 
-    tool = _make_tool_for_testing("test tool output")
-    runnable = get_runnable(session, tool)
-
-    result = runnable.invoke("test")
     assert result.output == "ai response"
     assert tool.func.call_args == (("test arg",), {})
     assert submit_tool_outputs.call_args == (
@@ -63,34 +57,25 @@ def test_assistant_tool_response(
 
 
 @patch("apps.assistants.sync.create_files_remote")  # called when tool output is being processed
-@patch("apps.service_providers.llm_service.runnables.AssistantChat._get_output_with_annotations")
 @patch("openai.resources.beta.threads.messages.Messages.create")  # called when tool output is submitted
-@patch("openai.resources.beta.threads.messages.Messages.list")  # called in runnable to get run messages
-@patch("openai.resources.beta.threads.runs.Runs.retrieve")  # called when 'waiting for run'
 @patch("openai.resources.beta.threads.runs.Runs.create")  # called when tool output is submitted
-@patch("openai.resources.beta.Threads.create_and_run")  # called during initial invoke
 @pytest.mark.django_db()
 def test_assistant_tool_artifact_response(
-    create_and_run,
     create_run,
-    retrieve_run,
-    list_messages,
     create_message,
-    get_output_with_annotations,
     create_files_remote,
     session,
 ):
-    run = configure_mocks(session, create_and_run, retrieve_run, list_messages, get_output_with_annotations)
-    create_run.return_value = run
+    with configure_common_mocks(session) as run:
+        create_run.return_value = run
 
-    # mock the tool so that it returns an artifact. This should trigger the file upload workflow
-    artifact = ToolArtifact(content=b"test artifact", filename="test.txt", content_type="text/plain")
-    tool = _make_tool_for_testing(("test response", artifact), response_format="content_and_artifact")
-    runnable = get_runnable(session, tool)
+        # mock the tool so that it returns an artifact. This should trigger the file upload workflow
+        artifact = ToolArtifact(content=b"test artifact", filename="test.txt", content_type="text/plain")
+        tool = _make_tool_for_testing(("test response", artifact), response_format="content_and_artifact")
+        runnable = get_runnable(session, tool)
+        create_files_remote.return_value = ["file-123abc"]
 
-    create_files_remote.return_value = ["file-123abc"]
-
-    result = runnable.invoke("test")
+        result = runnable.invoke("test")
 
     assert result.output == "ai response"
     assert tool.func.call_args == (("test arg",), {})
@@ -105,34 +90,43 @@ def test_assistant_tool_artifact_response(
     )
 
 
-def configure_mocks(session, create_and_run, retrieve_run, list_messages, get_output_with_annotations):
-    get_output_with_annotations.return_value = ("ai response", {})
-    thread_id = "test_thread_id"
-    assistant_id = session.experiment.assistant.assistant_id
-    run = _create_run(
-        assistant_id,
-        thread_id,
-        status="requires_action",
-        required_action=RequiredAction(
-            submit_tool_outputs=RequiredActionSubmitToolOutputs(
-                tool_calls=[
-                    RequiredActionFunctionToolCall(
-                        id="call1",
-                        function=Function(name="fake_tool", arguments='{"arg1": "test arg"}'),
-                        type="function",
-                    )
-                ]
+@contextmanager
+def configure_common_mocks(session):
+    with (
+        patch(
+            "apps.service_providers.llm_service.runnables.AssistantChat._get_output_with_annotations"
+        ) as get_output_with_annotations,
+        patch("openai.resources.beta.threads.messages.Messages.list") as list_messages,
+        patch("openai.resources.beta.threads.runs.Runs.retrieve") as retrieve_run,
+        patch("openai.resources.beta.Threads.create_and_run") as create_and_run,
+    ):
+        get_output_with_annotations.return_value = ("ai response", {})
+        thread_id = "test_thread_id"
+        assistant_id = session.experiment.assistant.assistant_id
+        run = _create_run(
+            assistant_id,
+            thread_id,
+            status="requires_action",
+            required_action=RequiredAction(
+                submit_tool_outputs=RequiredActionSubmitToolOutputs(
+                    tool_calls=[
+                        RequiredActionFunctionToolCall(
+                            id="call1",
+                            function=Function(name="fake_tool", arguments='{"arg1": "test arg"}'),
+                            type="function",
+                        )
+                    ]
+                ),
+                type="submit_tool_outputs",
             ),
-            type="submit_tool_outputs",
-        ),
-    )
-    create_and_run.return_value = run
-    # 1st call on initial invoke, 2nd after tool message
-    retrieve_run.side_effect = [run, _create_run(assistant_id, thread_id)]
-    list_messages.return_value = _create_thread_messages(
-        assistant_id, run.id, thread_id, [{"assistant": "ai response"}]
-    )
-    return run
+        )
+        create_and_run.return_value = run
+        # 1st call on initial invoke, 2nd after tool message
+        retrieve_run.side_effect = [run, _create_run(assistant_id, thread_id)]
+        list_messages.return_value = _create_thread_messages(
+            assistant_id, run.id, thread_id, [{"assistant": "ai response"}]
+        )
+        yield run
 
 
 def get_runnable(session, tool):
