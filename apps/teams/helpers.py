324c92e2da3b787a5790607a6db0e11c6ea80097
Simon Kelly <skelly@dimagi.com>
2024-12-12 17:30:21 +0200

    set fake membership for temp superuser access

--- apps/teams/helpers.py
+++ apps/teams/helpers.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext as _
 
 from apps.users.models import CustomUser
 from apps.utils.slug import get_next_unique_slug
+from apps.web.superuser_utils import has_temporary_superuser_access
 
 from .backends import make_user_team_owner
 from .models import Membership, Team
@@ -62,4 +63,18 @@ def create_default_team_for_user(user: CustomUser, team_name: str = None):
 
 def get_team_membership_for_request(request: HttpRequest):
     if request.user.is_authenticated and request.team:
-        return Membership.objects.filter(team=request.team, user=request.user).first()
+        membership = Membership.objects.filter(team=request.team, user=request.user).first()
+        if not membership and request.user.is_superuser and has_temporary_superuser_access(request, request.team.slug):
+            membership = SuperuserMembership(request.user, request.team)
+        return membership
+
+
+class SuperuserMembership:
+    """Dummy membership for superusers who have temporary access to a team."""
+
+    def __init__(self, user: CustomUser, team: Team):
+        self.user = user
+        self.team = team
+
+    def is_team_admin(self):
+        return self.user.is_superuser
