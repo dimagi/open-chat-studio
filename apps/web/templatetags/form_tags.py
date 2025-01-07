0c8345b09ccf732f9b84cb5a5317415e8dd460f5
Simon Kelly <skelly@dimagi.com>
2025-01-07 20:40:27 +0200

    extract templates to files

--- apps/web/templatetags/form_tags.py
+++ apps/web/templatetags/form_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.template import engines
+from django.template.loader import get_template
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -30,72 +32,27 @@ def render_field(form_field, **attrs):
 
 @register.simple_tag
 def render_text_input(form_field, **attrs):
-    TEXT_INPUT_TEMPLATE = """
-    <div class="form-control w-full" {% include "generic/attrs.html" with attrs=control_attrs %}>
-      <label class="label font-bold" for="{{ form_field.id_for_label }}">
-      <div>
-        {{ form_field.label }}
-        {% if form_field.help_text %}
-        <small class="form-text text-muted">{% include "generic/help.html" with help_content=form_field.help_text %}</small>
-        {% endif %}
-      </div>
-      </label>
-      {{ form_field }}
-      {{ form_field.errors }}
-    </div>
-    """
-    return _render_field(TEXT_INPUT_TEMPLATE, form_field, **attrs)
+    return _render_field("web/form/text.html", form_field, **attrs)
 
 
 @register.simple_tag
 def render_select_input(form_field, **attrs):
-    SELECT_INPUT_TEMPLATE = """
-    <div class="form-control w-full" {% include "generic/attrs.html" with attrs=control_attrs %}>
-      <label class="label font-bold" for="{{ form_field.id_for_label }}">
-      <div>
-        {{ form_field.label }}
-        {% if form_field.help_text %}
-        <small class="form-text text-muted">{% include "generic/help.html" with help_content=form_field.help_text %}</small>
-        {% endif %}
-      </div>
-      </label>
-      {{ form_field }}
-      {{ form_field.errors }}
-    </div>
-    """
-    return _render_field(SELECT_INPUT_TEMPLATE, form_field, **attrs)
+    return _render_field("web/form/select.html", form_field, **attrs)
 
 
 @register.simple_tag
 def render_checkbox_input(form_field, **attrs):
-    CHECKBOX_INPUT_TEMPLATE = """
-    <div class="form-control" {% include "generic/attrs.html" with attrs=control_attrs %}>
-      <div class="form-check">
-        <label class="label font-bold cursor-pointer">
-        <div>
-          <span class="label-text">{{ form_field.label }}</span>
-          {% if form_field.help_text %}
-          <small class="form-text text-muted">{% include "generic/help.html" with help_content=form_field.help_text %}</small>
-          {% endif %}
-        </div>
-          {{ form_field }}
-        </label>
-      </div>
-      {{ form_field.errors }}
-    </div>
-    """
-    return _render_field(CHECKBOX_INPUT_TEMPLATE, form_field, **attrs)
+    return _render_field("web/form/checkbox.html", form_field, **attrs)
 
 
-def _render_field(template_text, form_field, **attrs):
+def _render_field(template_name, form_field, **attrs):
     control_attrs = form_field.field.widget.attrs.pop("control_attrs", None) or {}
     if not form_field.is_hidden:
-        template_object = template.Template(template_text)
+        template_object = get_template(template_name)
     else:
-        template_object = template.Template("{{ form_field }}")
+        template_object = engines.all()[0].from_string("{{ form_field }}")
     control_attrs = control_attrs | _transform_x_attrs(attrs)
-    context = template.Context({"form_field": form_field, "control_attrs": control_attrs})
-    return template_object.render(context)
+    return template_object.render({"form_field": form_field, "control_attrs": control_attrs})
 
 
 def _transform_x_attrs(attrs):
