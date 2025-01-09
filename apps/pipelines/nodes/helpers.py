2080aa1c3f49904376fe42314a07bd6e7272cb30
Chris Smit <chris.smit@dimagi.com>
2025-01-09 10:22:03 +0200

    Set current team in the temporary_session context manager

--- apps/pipelines/nodes/helpers.py
+++ apps/pipelines/nodes/helpers.py
@@ -6,27 +6,29 @@ from apps.channels.models import ChannelPlatform, ExperimentChannel
 from apps.chat.models import Chat
 from apps.experiments.models import ConsentForm, Experiment, ExperimentSession, Participant
 from apps.teams.models import Team
+from apps.teams.utils import current_team
 from apps.users.models import CustomUser
 
 
 @contextmanager
 def temporary_session(team: Team, user_id: int):
     """A temporary sesssion setup that is rolled back after the context exits."""
-    with transaction.atomic():
-        user = CustomUser.objects.get(id=user_id)
-        consent_form = ConsentForm.get_default(team)
-        experiment = Experiment.objects.create(
-            team=team, name="Temporary Experiment", owner=user, consent_form=consent_form
-        )
-        channel = ExperimentChannel.objects.get_team_web_channel(team)
-        chat = Chat.objects.create(team=team, name="Temporary Chat")
-        participant, _ = Participant.objects.get_or_create(user=user, team=team, platform=ChannelPlatform.WEB)
-        experiment_session = ExperimentSession.objects.create(
-            team=team,
-            experiment=experiment,
-            chat=chat,
-            experiment_channel=channel,
-            participant=participant,
-        )
-        yield experiment_session
-        transaction.set_rollback(True)
+    with current_team(team):
+        with transaction.atomic():
+            user = CustomUser.objects.get(id=user_id)
+            consent_form = ConsentForm.get_default(team)
+            experiment = Experiment.objects.create(
+                team=team, name="Temporary Experiment", owner=user, consent_form=consent_form
+            )
+            channel = ExperimentChannel.objects.get_team_web_channel(team)
+            chat = Chat.objects.create(team=team, name="Temporary Chat")
+            participant, _ = Participant.objects.get_or_create(user=user, team=team, platform=ChannelPlatform.WEB)
+            experiment_session = ExperimentSession.objects.create(
+                team=team,
+                experiment=experiment,
+                chat=chat,
+                experiment_channel=channel,
+                participant=participant,
+            )
+            yield experiment_session
+            transaction.set_rollback(True)
