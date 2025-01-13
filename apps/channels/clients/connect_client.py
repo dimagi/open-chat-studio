9ac81bdb3ac883ba720a06291ead213115d0409a
Chris Smit <chris.smit@dimagi.com>
2025-01-13 11:57:21 +0200

    Add retry logic to the create_channel call

--- apps/channels/clients/connect_client.py
+++ apps/channels/clients/connect_client.py
@@ -1,8 +1,12 @@
+import logging
 from uuid import UUID
 
 import requests
 from django.conf import settings
 from requests.auth import HTTPBasicAuth
+from tenacity import before_sleep_log, retry, retry_if_exception_type, stop_after_attempt, wait_exponential
+
+logger = logging.getLogger("connectid-api")
 
 
 class ConnectClient:
@@ -10,6 +14,13 @@ class ConnectClient:
         self._base_url = settings.CONNECT_ID_SERVER_BASE_URL
         self._auth = HTTPBasicAuth(settings.CONNECT_MESSAGING_SERVER_ID, settings.CONNECT_MESSAGING_SERVER_SECRET)
 
+    @retry(
+        wait=wait_exponential(multiplier=1, min=1, max=5),
+        reraise=True,
+        retry=retry_if_exception_type(requests.ConnectionError),
+        stop=stop_after_attempt(3),
+        before_sleep=before_sleep_log(logger, logging.INFO),
+    )
     def create_channel(self, connect_id: UUID, channel_source: str) -> UUID:
         url = f"{self._base_url}/messaging/create_channel"
         response = requests.post(
