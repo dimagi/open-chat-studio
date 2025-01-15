6ca9083dfb9cc5d07dfb0419689bb8e398b2e187
Simon Kelly <skelly@dimagi.com>
2025-01-15 15:37:40 +0200

    don't error on build errors during pipeline test

--- apps/pipelines/tasks.py
+++ apps/pipelines/tasks.py
@@ -2,6 +2,7 @@ from celery.app import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
 
+from apps.pipelines.exceptions import PipelineBuildError
 from apps.pipelines.models import Pipeline
 
 
@@ -20,4 +21,7 @@ def send_email_from_pipeline(recipient_list, subject, message):
 @shared_task
 def get_response_for_pipeline_test_message(pipeline_id: int, message_text: str, user_id: int):
     pipeline = Pipeline.objects.get(id=pipeline_id)
-    return pipeline.simple_invoke(message_text, user_id=user_id)
+    try:
+        return pipeline.simple_invoke(message_text, user_id)
+    except PipelineBuildError as e:
+        return {"error": str(e)}
