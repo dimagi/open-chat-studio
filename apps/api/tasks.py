e0c26440bb32088ee34eaf0d6832aba87cce8606
Chris Smit <64970009+SmittieC@users.noreply.github.com>
2025-01-15 10:46:27 +0200

    Update apps/api/tasks.py
    Co-authored-by: Simon Kelly <skelly@dimagi.com>

--- apps/api/tasks.py
+++ apps/api/tasks.py
@@ -66,7 +66,7 @@ def trigger_bot_message_task(data):
     identifier = data["identifier"]
 
     experiment = Experiment.objects.get(public_id=experiment_public_id)
-    experiment_channel = ExperimentChannel.objects.prefetch_related("experiment").get(
+    experiment_channel = ExperimentChannel.objects.get(
         platform=platform, experiment=experiment
     )
 
