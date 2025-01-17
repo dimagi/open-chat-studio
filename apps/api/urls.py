a81667a6db6b38c9d2a05215dd673e8727375ef3
Chris Smit <chris.smit@dimagi.com>
2025-01-17 09:29:34 +0200

    Remove trailing slashes

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -10,9 +10,9 @@ router.register(r"experiments", views.ExperimentViewSet, basename="experiment")
 router.register(r"sessions", views.ExperimentSessionViewSet, basename="session")
 
 connect_patterns = [
-    path("generate_key/", views.generate_key, name="generate_key"),
-    path("callback/", views.callback, name="callback"),
-    path("consent/", views.consent, name="consent"),
+    path("generate_key", views.generate_key, name="generate_key"),
+    path("callback", views.callback, name="callback"),
+    path("consent", views.consent, name="consent"),
 ]
 
 urlpatterns = [
@@ -20,6 +20,6 @@ urlpatterns = [
     path("openai/<uuid:experiment_id>/chat/completions", openai.chat_completions, name="openai-chat-completions"),
     path("files/<int:pk>/content", views.file_content_view, name="file-content"),
     path("commcare_connect/", include((connect_patterns, "commcare-connect"))),
-    path("trigger_bot/", views.trigger_bot_message, name="trigger_bot"),
+    path("trigger_bot", views.trigger_bot_message, name="trigger_bot"),
     path("", include(router.urls)),
 ]
