7994f99a1adcf057be650e3a74099bef09000192
Simon Kelly <skelly@dimagi.com>
2025-02-06 11:39:14 +0200

    update UI to use 'share dialog' with embed tab

--- apps/web/templatetags/default_tags.py
+++ apps/web/templatetags/default_tags.py
@@ -11,3 +11,9 @@ def define(val=None):
 @register.filter(name="times")
 def times(number):
     return range(number)
+
+
+@register.simple_tag(takes_context=True)
+def absolute_url(context, relative_url):
+    request = context["request"]
+    return request.build_absolute_uri(relative_url)
