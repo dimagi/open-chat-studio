614179f43822abdaff13ed1ae8f8f1ac528ec4ac
Steph Herbers <stephherbers@gmail.com>
2025-02-18 08:08:17 -0500

    use with current_team(team) to set team for auditing task

--- apps/teams/tasks.py
+++ apps/teams/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 
 from apps.teams.invitations import send_invitation_accepted
 from apps.teams.models import Invitation, Team
+from apps.teams.utils import current_team
 from apps.utils.deletion import (
     delete_object_with_auditing_of_related_objects,
     get_admin_emails_with_delete_permission,
@@ -18,9 +19,9 @@ def send_invitation_accepted_notification(invitation_id):
 @shared_task
 def delete_team_async(team_id):
     team = Team.objects.get(id=team_id)
-
-    # get required info for emails prior to deleting team info
-    team_name = team.name
-    admin_emails = get_admin_emails_with_delete_permission(team)
-    delete_object_with_auditing_of_related_objects(team)
-    send_team_deleted_notification(team_name, admin_emails)
+    with current_team(team):
+        # get required info for emails prior to deleting team info
+        team_name = team.name
+        admin_emails = get_admin_emails_with_delete_permission(team)
+        delete_object_with_auditing_of_related_objects(team)
+        send_team_deleted_notification(team_name, admin_emails)
