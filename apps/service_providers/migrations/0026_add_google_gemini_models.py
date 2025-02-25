3dd3c512c1aa8663227d65f9d30a4ef2645459ed
vaibhavK1199 <vdattatraykumbhar@dimagi-associate.com>
2025-02-25 12:35:17 +0530

    modified migrations

--- apps/service_providers/migrations/0026_add_google_gemini_models.py
+++ apps/service_providers/migrations/0026_add_google_gemini_models.py
@@ -1,9 +1,10 @@
 import logging
-from django.db import migrations
+from django.db import migrations, models
 
 logger = logging.getLogger(__name__)
 
 def add_google_gemini_models(apps, schema_editor):
+    """Function to update LlmProviderModel with Google Gemini."""
     from apps.service_providers.llm_service.default_models import _update_llm_provider_models
     LlmProviderModel = apps.get_model("service_providers", "LlmProviderModel")
     try:
@@ -13,9 +14,38 @@ def add_google_gemini_models(apps, schema_editor):
         raise
 
 class Migration(migrations.Migration):
+
     dependencies = [
-        ('service_providers', '0025_alter_llmprovider_type_alter_llmprovidermodel_type'),  # Ensure it runs after DeepSeek migration
+        ('service_providers', '0025_alter_llmprovider_type_alter_llmprovidermodel_type'),
     ]
+
     operations = [
         migrations.RunPython(add_google_gemini_models),
+
+        migrations.AlterField(
+            model_name='llmprovider',
+            name='type',
+            field=models.CharField(choices=[
+                ('openai', 'OpenAI'),
+                ('azure', 'Azure OpenAI'),
+                ('anthropic', 'Anthropic'),
+                ('groq', 'Groq'),
+                ('perplexity', 'Perplexity'),
+                ('deepseek', 'DeepSeek'),
+                ('google', 'Google Gemini')
+            ], max_length=255),
+        ),
+        migrations.AlterField(
+            model_name='llmprovidermodel',
+            name='type',
+            field=models.CharField(choices=[
+                ('openai', 'OpenAI'),
+                ('azure', 'Azure OpenAI'),
+                ('anthropic', 'Anthropic'),
+                ('groq', 'Groq'),
+                ('perplexity', 'Perplexity'),
+                ('deepseek', 'DeepSeek'),
+                ('google', 'Google Gemini')
+            ], max_length=255),
+        ),
     ]
