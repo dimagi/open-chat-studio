44871972dd07b5814f7a0e65795bc9fbc568316d
Steph Herbers <stephherbers@gmail.com>
2025-02-26 08:54:43 -0500

    modify truncate_dict_items to truncate_dict_values to include all the keys, but shorten all values if they are beyond a certain limit

--- apps/events/utils.py
+++ apps/events/utils.py
@@ -1,18 +1,13 @@
 def truncate_dict_items(dictionary, max_limit=100):
     """
-    Truncate dictionary items to fit within a char limit. Returns list of (key, value)
-    tuples that fit within the limit without breaking up the key value
+    Truncate dictionary values to fit within a char limit for each value.
     """
     formatted_items = []
-    chars_used = 0
     for key, val in dictionary.items():
         key_str, val_str = str(key), str(val)
-        if chars_used + len(key_str) > max_limit:
-            break
-        space_for_value = max_limit - chars_used - len(key_str)
-        truncated = len(val_str) > space_for_value
-        formatted_items.append((key_str, val_str[:space_for_value] + ("..." if truncated else "")))
-        if truncated:
-            break
-        chars_used += len(key_str) + len(val_str)
+        if len(val_str) > max_limit:
+            truncated_val = val_str[:max_limit] + "..."
+        else:
+            truncated_val = val_str
+        formatted_items.append((key_str, truncated_val))
     return formatted_items
