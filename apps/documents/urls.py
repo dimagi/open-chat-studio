c0576a2e1bc741be223ff1d0677c30edb2040ec3
Chris Smit <chris.smit@dimagi.com>
2025-02-27 16:27:34 +0200

    Ability to delete a file

--- apps/documents/urls.py
+++ apps/documents/urls.py
@@ -6,11 +6,12 @@ app_name = "documents"
 
 urlpatterns = [
     path("<slug:tab_name>", views.RepositoryHome.as_view(), name="repositories"),
-    # List views
+    # Files
     path("files/", views.FileListView.as_view(), name="files_list"),
     path("files/<int:id>/details", views.FileDetails.as_view(), name="file_details"),
     path("files/upload", views.upload_files, name="upload_files"),
-    # Detail views
+    path("files/<int:id>/delete", views.delete_file, name="delete_file"),
+    # Collections
     path("collections/", views.CollectionListView.as_view(), name="collections_list"),
     path("collections/<int:id>/details", views.CollectionDetails.as_view(), name="collection_details"),
 ]
