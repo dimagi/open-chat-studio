cf887c9d222c8f6a87bf3c058dee3ec531407ef1
Chris Smit <chris.smit@dimagi.com>
2025-02-27 15:51:46 +0200

    Add upload_file modal
    Make it generic so the collections and future tabs can also use this

--- apps/documents/views.py
+++ apps/documents/views.py
@@ -1,6 +1,12 @@
+import json
+from functools import cache
+
 from django.contrib.postgres.search import TrigramSimilarity
+from django.db import transaction
 from django.db.models import Q
+from django.shortcuts import redirect
 from django.urls import reverse
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView, TemplateView
 
 from apps.documents.models import Repository, RepositoryType
@@ -12,13 +18,20 @@ class RepositoryHome(LoginAndTeamRequiredMixin, TemplateView):
     template_name = "documents/repositories.html"
 
     def get_context_data(self, team_slug: str, tab_name: str, **kwargs):
-        return {
+        context = {
             "active_tab": "manage_files",
             "title": "Manage Files",
             "tab_name": tab_name,
             "files_list_url": reverse("documents:files_list", kwargs={"team_slug": team_slug}),
+            "upload_files_url": reverse("documents:upload_files", kwargs={"team_slug": team_slug}),
             "collections_list_url": reverse("documents:collections_list", kwargs={"team_slug": team_slug}),
         }
+        if tab_name == "files":
+            context["collections"] = Repository.objects.filter(
+                team__slug=team_slug, type=RepositoryType.COLLECTION
+            ).all()
+
+        return context
 
 
 class BaseObjectListView(ListView):
@@ -32,8 +45,12 @@ class BaseObjectListView(ListView):
 
 
 class BaseDetailsView(TemplateView):
+    @cache
+    def get_object(self):
+        return self.model.objects.get(team__slug=self.kwargs["team_slug"], id=self.kwargs["id"])
+
     def get_context_data(self, team_slug: str, id, **kwargs):
-        return {"object": self.model.objects.get(team__slug=team_slug, id=id)}
+        return {"object": self.get_object()}
 
 
 class FileListView(LoginAndTeamRequiredMixin, BaseObjectListView):
@@ -58,6 +75,36 @@ class FileListView(LoginAndTeamRequiredMixin, BaseObjectListView):
 class FileDetails(LoginAndTeamRequiredMixin, BaseDetailsView):
     template_name = "documents/file_details.html"
     model = File
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        file = self.get_object()
+        collection_names = file.repository_set.filter(type=RepositoryType.COLLECTION).values_list("name", flat=True)
+        context["collections"] = ", ".join(collection_names)
+        return context
+
+
+@require_POST
+@transaction.atomic()
+def upload_files(request, team_slug: str):
+    """Upload files to a collection"""
+    # TODO: Check collection size and error if it's too large with the new files added
+    files = []
+    file_summaries = json.loads(request.POST["file_summaries"])
+    for uploaded_file in request.FILES.getlist("files"):
+        files.append(
+            File.objects.create(
+                team=request.team,
+                name=uploaded_file.name,
+                file=uploaded_file,
+                summary=file_summaries[uploaded_file.name],
+            )
+        )
+    repo, _ = Repository.objects.get_or_create(
+        type=RepositoryType.COLLECTION, team=request.team, name=request.POST.get("collection_name")
+    )
+    repo.files.add(*files)
+    return redirect(reverse("documents:repositories", kwargs={"team_slug": team_slug, "tab_name": "files"}))
 
 
 class CollectionListView(LoginAndTeamRequiredMixin, BaseObjectListView):
