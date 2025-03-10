0d35e3bcc7458fd841d4c9fa321571ecc6528191
Chris Smit <chris.smit@dimagi.com>
2025-03-10 12:50:10 +0200

    Show files beloning to a collection

--- apps/documents/models.py
+++ apps/documents/models.py
@@ -19,4 +19,7 @@ class Repository(BaseTeamModel):
     @property
     def size(self) -> float:
         """Returns the size of this repository in megabytes"""
-        return sum([size_bytes for size_bytes in self.files.values_list("content_size", flat=True)]) / 1048576
+        return round(sum([size_bytes for size_bytes in self.files.values_list("content_size", flat=True)]) / 1048576, 2)
+
+    def file_names(self) -> list[str]:
+        return list(self.files.values_list("name", flat=True))
