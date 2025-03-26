7504020a5970cfc2efd5b6888620966a37164dac
Simon Kelly <skelly@dimagi.com>
2025-03-26 10:34:51 +0200

    add security decorators and checks

--- apps/experiments/views/chat.py
+++ apps/experiments/views/chat.py
@@ -2,13 +2,15 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 from apps.chat.models import ChatMessage, ChatMessageType
+from apps.teams.decorators import team_required
 
 
+@team_required
 def rate_message(request, team_slug: str, message_id: int, rating: str):
     if rating not in ["👍", "👎"]:
         raise Http404()
 
-    message = get_object_or_404(ChatMessage, id=message_id, message_type=ChatMessageType.AI)
+    message = get_object_or_404(ChatMessage, id=message_id, message_type=ChatMessageType.AI, chat__team=request.team)
     message.add_rating(rating)
 
     return render(
