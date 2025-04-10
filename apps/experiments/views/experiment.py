44eee7dc6c3b6b746694daa5ed9ec2547ed44061
Simon Kelly <skelly@dimagi.com>
2025-04-10 14:30:48 +0200

    fix polling for embedded chat

--- apps/experiments/views/experiment.py
+++ apps/experiments/views/experiment.py
@@ -23,7 +23,7 @@ from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_GET, require_POST
 from django.views.generic import CreateView, UpdateView
 from django_tables2 import SingleTableView
 from field_audit.models import AuditAction
@@ -813,14 +813,31 @@ def get_message_response(request, team_slug: str, experiment_id: uuid.UUID, sess
     )
 
 
+@experiment_session_view()
+@require_GET
+@xframe_options_exempt
 @team_required
-def poll_messages(request, team_slug: str, experiment_id: int, session_id: int):
+def poll_messages_embed(request, team_slug: str, experiment_id: uuid.UUID, session_id: str):
+    if not request.experiment_session.participant.is_anonymous:
+        return HttpResponseForbidden()
+
+    return _poll_messages(request)
+
+
+@experiment_session_view()
+@require_GET
+@team_required
+def poll_messages(request, team_slug: str, experiment_id: uuid.UUID, session_id: str):
     user = get_real_user_or_none(request.user)
+    if not request.experiment_session.participant.user == user:
+        return HttpResponseForbidden()
+
+    return _poll_messages(request)
+
+
+def _poll_messages(request):
     params = request.GET.dict()
     since_param = params.get("since")
-    experiment_session = get_object_or_404(
-        ExperimentSession, participant__user=user, experiment_id=experiment_id, id=session_id, team=request.team
-    )
 
     since = timezone.now()
     if since_param and since_param != "null":
@@ -830,7 +847,9 @@ def poll_messages(request, team_slug: str, experiment_id: int, session_id: int):
             logging.exception(f"Unexpected `since` parameter value. Error: {e}")
 
     messages = (
-        ChatMessage.objects.filter(message_type=ChatMessageType.AI, chat=experiment_session.chat, created_at__gt=since)
+        ChatMessage.objects.filter(
+            message_type=ChatMessageType.AI, chat=request.experiment_session.chat, created_at__gt=since
+        )
         .order_by("created_at")
         .all()
     )
