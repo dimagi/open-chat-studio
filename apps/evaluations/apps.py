eb8b69cb5ce78a63e66aa3c100283678919c7e35
Farid Rener <proteusvacuum@gmail.com>
2025-04-17 09:37:56 -0400

    Basic list view

--- apps/evaluations/apps.py
+++ apps/evaluations/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class EvaluationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.evaluations"
+    label = "evaluations"
