44d4bef1e1667c98afbc8bd2d74dd0e769cc2ba5
Farid Rener <proteusvacuum@gmail.com>
2025-04-18 08:21:12 -0400

    Add rudimentary django admin

--- apps/evaluations/admin.py
+++ apps/evaluations/admin.py
@@ -1,1 +1,57 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import (
+    EvaluationConfig,
+    EvaluationDataset,
+    EvaluationResult,
+    EvaluationRun,
+    Evaluator,
+)
+
+
+@admin.register(Evaluator)
+class EvaluatorAdmin(admin.ModelAdmin):
+    list_display = ("id", "type", "team")
+    list_filter = ("type", "team")
+    search_fields = ("type",)
+
+
+@admin.register(EvaluationDataset)
+class EvaluationDatasetAdmin(admin.ModelAdmin):
+    list_display = ("id", "version", "message_type", "team")
+    list_filter = ("message_type", "version", "team")
+    filter_horizontal = ("sessions",)
+    search_fields = ("version__version_number",)
+
+
+@admin.register(EvaluationConfig)
+class EvaluationConfigAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "experiment", "dataset", "team")
+    list_filter = ("experiment", "team")
+    filter_horizontal = ("evaluators",)
+    search_fields = (
+        "name",
+        "experiment__version_number",
+    )
+
+
+class EvaluationResultInline(admin.TabularInline):
+    model = EvaluationResult
+    extra = 0
+    readonly_fields = ("evaluator", "output")
+
+
+@admin.register(EvaluationRun)
+class EvaluationRunAdmin(admin.ModelAdmin):
+    list_display = ("id", "config", "created_at", "finished_at", "user", "team")
+    list_filter = ("config__experiment", "team")
+    readonly_fields = ("created_at", "finished_at")
+    inlines = (EvaluationResultInline,)
+
+
+@admin.register(EvaluationResult)
+class EvaluationResultAdmin(admin.ModelAdmin):
+    list_display = ("id", "run", "evaluator", "team")
+    list_filter = ("evaluator", "run__config__experiment", "team")
+    readonly_fields = ("output",)
+    search_fields = ("output",)
