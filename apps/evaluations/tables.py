a286442e84529146b46a8559d6bd82adb8d8fc15
Farid Rener <proteusvacuum@gmail.com>
2025-04-23 13:25:46 -0400

    Add views for datasets

--- apps/evaluations/tables.py
+++ apps/evaluations/tables.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django_tables2 import columns, tables
 
-from apps.evaluations.models import EvaluationConfig, EvaluationRun, Evaluator
+from apps.evaluations.models import EvaluationConfig, EvaluationDataset, EvaluationRun, Evaluator
 from apps.generics import actions
 
 
@@ -96,3 +96,33 @@ class EvaluatorTable(tables.Table):
         row_attrs = settings.DJANGO_TABLES2_ROW_ATTRS
         orderable = False
         empty_text = "No evaluators found."
+
+
+class EvaluationDatasetTable(tables.Table):
+    actions = actions.ActionsColumn(
+        actions=[
+            actions.edit_action(url_name="evaluations:dataset_edit"),
+        ]
+        # actions=[
+        #     actions.edit_action(url_name="pipelines:edit"),
+        #     actions.AjaxAction(
+        #         "pipelines:delete",
+        #         title="Archive",
+        #         icon_class="fa-solid fa-box-archive",
+        #         required_permissions=["pipelines.delete_pipeline"],
+        #         confirm_message="This will delete the pipeline and any associated logs. Are you sure?",
+        #         hx_method="delete",
+        #     ),
+        # ]
+    )
+
+    class Meta:
+        model = EvaluationDataset
+        fields = (
+            "name",
+            "message_type",
+            "sessions",
+        )
+        row_attrs = settings.DJANGO_TABLES2_ROW_ATTRS
+        orderable = False
+        empty_text = "No datasets found."
