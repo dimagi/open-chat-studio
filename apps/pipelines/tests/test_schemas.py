bbff42175aafbf4b84458b05ea3ef9e54dd9a1ab
Simon Kelly <skelly@dimagi.com>
2025-04-24 11:28:54 +0200

    ensure all nodes are loaded in view

--- apps/pipelines/tests/test_schemas.py
+++ apps/pipelines/tests/test_schemas.py
@@ -3,13 +3,14 @@ import pathlib
 
 from apps.pipelines.views import _pipeline_node_schemas
 
-BASE = pathlib.Path(__file__).parent / "data"
+BASE = pathlib.Path(__file__).parent / "node_schemas"
 
 
 def test_schemas():
     schemas = _pipeline_node_schemas()
     for schema in schemas:
         title = schema["title"]
+        print(title)
         assert schema["description"], title
         assert schema["ui:label"], title
 
@@ -18,3 +19,11 @@ def test_schemas():
             raise AssertionError(
                 f"Pipeline schema for {title} has changed. Run 'python manage.py update_pipeline_schema'."
             )
+
+
+def test_pipeline_node_schemas():
+    schemas = {schema["title"] for schema in _pipeline_node_schemas()}
+    for file in BASE.glob("*.json"):
+        node_name = file.name.split(".")[0]
+        if node_name not in schemas:
+            raise AssertionError(f"Schema found for unknown node: {file.name}.")
