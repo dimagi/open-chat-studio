a685651e3559441d63e07e6d6b2a4308e6849879
Chris Smit <chris.smit@dimagi.com>
2025-05-01 13:22:38 +0200

    Require llm provider when is_index is True

--- apps/documents/forms.py
+++ apps/documents/forms.py
@@ -13,7 +13,7 @@ class CollectionForm(forms.ModelForm):
             "is_index": "Create file index",
         }
         help_texts = {
-            "is_index": "If checked, the files will be indexed and searchable using RAG.",
+            "is_index": "If checked, the files will be indexed and searchable using RAG",
             "llm_provider": "This is the LLM provider at which the vector store will be created.",
         }
 
@@ -21,10 +21,22 @@ class CollectionForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields["llm_provider"].queryset = request.team.llmprovider_set.filter(type=LlmProviderTypes.openai).all()
 
+        self.fields["is_index"].widget.attrs = {"x-model": "isIndex"}
         if self.instance.id:
             self.fields["is_index"].widget.attrs["disabled"] = True
-        else:
-            self.fields["is_index"].widget.attrs = {"x-model": "isIndex"}
+
+        if self.instance.is_index:
+            self.fields["llm_provider"].widget.is_required = True
+
+    def clean_is_index(self):
+        if self.instance.id:
+            return self.instance.is_index
+        return self.cleaned_data["is_index"]
+
+    def clean_llm_provider(self):
+        if self.cleaned_data["is_index"] and not self.cleaned_data["llm_provider"]:
+            raise forms.ValidationError("This field is required when creating an index.")
+        return self.cleaned_data["llm_provider"]
 
 
 class FileForm(forms.ModelForm):
