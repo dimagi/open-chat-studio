2c38de1c0b7fbb209dcd06c7f2c72acff232ff91
Chris Smit <chris.smit@dimagi.com>
2025-05-07 15:10:27 +0200

    Update tool schemata to work with OpenAI's JSON schema

--- apps/chat/agent/schemas.py
+++ apps/chat/agent/schemas.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from enum import Enum
-from typing import Any
 
 from pydantic import BaseModel, Field
 
@@ -25,9 +24,9 @@ class RecurringReminderSchema(BaseModel):
     datetime_due: datetime = Field(description="The first (or only) reminder start date in ISO 8601 format")
     every: int = Field(description="Number of 'periods' to wait between reminders")
     period: TimePeriod = Field(description="The time period used in conjunction with 'every'")
-    datetime_end: datetime | None = Field(description="The date of the last reminder in ISO 8601 format", default=None)
+    datetime_end: datetime = Field(description="The date of the last reminder in ISO 8601 format", default=None)
     message: str = Field(description=REMINDER_MESSAGE_HELP_TEXT)
-    repetitions: str | None = Field(description="The number of repetitions", default=None)
+    repetitions: str = Field(description="The number of repetitions", default=None)
     schedule_name: str = Field(description="The name for this scheduled message")
 
 
@@ -46,12 +45,12 @@ class ScheduledMessageSchema(BaseModel):
     weekday: WeekdaysEnum = Field(description="The new day of the week")
     hour: int = Field(description="The new hour of the day, in UTC")
     minute: int = Field(description="The new minute of the hour")
-    specified_date: datetime | None = Field(description="True if the user requested a specific date", default=None)
+    specified_date: datetime = Field(description="True if the user requested a specific date", default=None)
 
 
 class UpdateUserDataSchema(BaseModel):
     key: str = Field(description="The key in the user data to update")
-    value: Any = Field(None, description="The new value of the user data")
+    value: str | int | dict | list = Field(description="The new value of the user data")
 
 
 class AttachMediaSchema(BaseModel):
