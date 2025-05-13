58b6900f77707c7f0617a0e5dc85ad7cef6ade88
Chris Smit <chris.smit@dimagi.com>
2025-05-13 12:00:36 +0200

    Version indexed collections as well

--- apps/pipelines/models.py
+++ apps/pipelines/models.py
@@ -492,6 +492,18 @@ class Node(BaseModel, VersionsMixin, CustomActionOperationMixin):
         from apps.documents.models import Collection
         from apps.pipelines.nodes.nodes import AssistantNode, LLMResponseWithPrompt
 
+        def _get_collection_version(param_name: str):
+            if not new_version.params.get(param_name):
+                return
+
+            collection = Collection.objects.get(id=new_version.params.get(param_name))
+
+            if not collection.has_versions or collection.compare_with_latest():
+                collection_version = collection.create_new_version()
+                return str(collection_version.id)
+            else:
+                return self.latest_version.params.get(param_name)
+
         new_version = super().create_new_version(save=False, is_copy=is_copy)
         if is_copy and new_flow_id:
             old_flow_id = new_version.flow_id
@@ -507,14 +519,8 @@ class Node(BaseModel, VersionsMixin, CustomActionOperationMixin):
                 new_version.params["assistant_id"] = str(assistant_version.id)
 
         if not is_copy and self.type == LLMResponseWithPrompt.__name__:
-            if collection_id := new_version.params.get("collection_id"):
-                collection = Collection.objects.get(id=collection_id)
-
-                if not collection.has_versions or collection.compare_with_latest():
-                    collection_version = collection.create_new_version()
-                    new_version.params["collection_id"] = str(collection_version.id)
-                else:
-                    new_version.params["collection_id"] = self.latest_version.params.get("collection_id")
+            new_version.params["collection_id"] = _get_collection_version(param_name="collection_id")
+            new_version.params["collection_index_id"] = _get_collection_version(param_name="collection_index_id")
 
             if source_material_id := new_version.params.get("source_material_id"):
                 source_material = SourceMaterial.objects.filter(id=source_material_id).first()
@@ -582,6 +588,10 @@ class Node(BaseModel, VersionsMixin, CustomActionOperationMixin):
                         value = OpenAiAssistant.objects.filter(id=value).first()
                 case "collection_id":
                     name = "media"
+                    if value:
+                        value = Collection.objects.filter(id=value).first()
+                case "collection_index_id":
+                    name = "Collection Index"
                     if value:
                         value = Collection.objects.filter(id=value).first()
                 case "source_material_id":
