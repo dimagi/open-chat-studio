7c582ec640d51b8e6f613e20f6d87a7c430deb03
Chris Smit <chris.smit@dimagi.com>
2025-05-15 15:01:34 +0200

    Use index_manager_mock fixture in tests

--- apps/documents/tests/conftest.py
+++ apps/documents/tests/conftest.py
@@ -9,5 +9,6 @@ from apps.service_providers.llm_service.index_managers import OpenAIVectorStoreM
 def index_manager_mock():
     index_manager = Mock(spec=OpenAIVectorStoreManager)
     with patch("apps.service_providers.models.LlmProvider.get_index_manager") as get_index_manager:
+        index_manager.client = Mock()
         get_index_manager.return_value = index_manager
         yield index_manager
