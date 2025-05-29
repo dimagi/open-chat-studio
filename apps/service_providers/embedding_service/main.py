8794218f3a7b1641b3401125a05d3e9774537633
Chris Smit <chris.smit@dimagi.com>
2025-05-29 10:02:35 +0200

    Type hints

--- apps/service_providers/embedding_service/main.py
+++ apps/service_providers/embedding_service/main.py
@@ -1,6 +1,6 @@
 class OpenAIEmbeddingService:
     openai_api_key: str
-    openai_api_base: str = None
-    openai_organization: str = None
+    openai_api_base: str | None = None
+    openai_organization: str | None = None
 
     # TODO: Method to create embeddings
