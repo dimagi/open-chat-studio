de8036267054c459be58d1212bbcaa8d6f8cda8d
Chris Smit <chris.smit@dimagi.com>
2025-05-30 10:56:27 +0200

    add EmbeddingProviderModel admin

--- apps/service_providers/admin.py
+++ apps/service_providers/admin.py
@@ -6,7 +6,14 @@ from apps.assistants.models import OpenAiAssistant
 from apps.experiments.models import Experiment
 from apps.pipelines.models import Node
 
-from .models import LlmProvider, LlmProviderModel, MessagingProvider, TraceProvider, VoiceProvider
+from .models import (
+    EmbeddingProviderModel,
+    LlmProvider,
+    LlmProviderModel,
+    MessagingProvider,
+    TraceProvider,
+    VoiceProvider,
+)
 
 
 @admin.register(LlmProvider)
@@ -76,5 +83,11 @@ class MessagingProviderAdmin(admin.ModelAdmin):
 
 @admin.register(TraceProvider)
 class TraceProviderAdmin(admin.ModelAdmin):
+    list_display = ("name", "team", "type")
+    list_filter = ("team", "type")
+
+
+@admin.register(EmbeddingProviderModel)
+class EmbeddingProviderModelAdmin(admin.ModelAdmin):
     list_display = ("name", "team", "type")
     list_filter = ("team", "type")
