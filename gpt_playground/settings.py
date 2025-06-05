2c5680fbde3f5d952489697a619e90343b841749
Simon Kelly <skelly@dimagi.com>
2025-06-05 13:15:02 +0200

    remove conn max age

--- gpt_playground/settings.py
+++ gpt_playground/settings.py
@@ -201,6 +201,7 @@ else:
     }
 
 db_options = DATABASES["default"].setdefault("OPTIONS", {})
+db_options.pop("CONN_MAX_AGE", None)  # remove connection age since it's not compatible with connection pooling
 db_options["pool"] = {
     "min_size": env.int("DJANGO_DATABASE_POOL_MIN_SIZE", default=2),
     "max_size": env.int("DJANGO_DATABASE_POOL_MAX_SIZE", default=10),
