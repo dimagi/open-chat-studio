bda76209bd0dcac8ac8ebdff87f99264b183aa57
Simon Kelly <skelly@dimagi.com>
2025-06-20 09:26:01 +0200

    update tests

--- apps/assistants/tests/test_sync.py
+++ apps/assistants/tests/test_sync.py
@@ -21,6 +21,7 @@ from apps.assistants.sync import (
     remove_files_from_tool,
     sync_from_openai,
 )
+from apps.chat.agent.tools import TOOL_CLASS_MAP
 from apps.experiments.models import AgentTools
 from apps.service_providers.llm_service.index_managers import OpenAIVectorStoreManager
 from apps.utils.factories.assistants import OpenAiAssistantFactory
@@ -95,7 +96,7 @@ def test_push_assistant_to_openai_update(mock_update, vs_retrieve, vs_files_list
 
     openai_files = FileObjectFactory.create_batch(2)
 
-    internal_tools = [tool_cls(experiment_session=None) for tool_cls in AgentTools.legacy_experiment_tools()]
+    internal_tools = [TOOL_CLASS_MAP[tool](experiment_session=None) for tool in AgentTools.legacy_experiment_tools()]
     with patch("openai.resources.Files.create", side_effect=openai_files) as mock_file_create:
         push_assistant_to_openai(local_assistant, internal_tools=internal_tools)
     assert mock_update.called
