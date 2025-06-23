e277bd95467e4240ae3d713d37deb8d6a6a24133
barora05 <barora@dimagi-associate.com>
2025-06-23 14:00:59 +0530

    remove instead of comment

--- apps/service_providers/migrations/0026_add_google_gemini_models.py
+++ apps/service_providers/migrations/0026_add_google_gemini_models.py
@@ -18,34 +18,3 @@ class Migration(migrations.Migration):
     dependencies = [
         ('service_providers', '0025_alter_llmprovider_type_alter_llmprovidermodel_type'),
     ]
-
-    # operations = [
-    #     migrations.RunPython(add_google_gemini_models),
-    #
-    #     migrations.AlterField(
-    #         model_name='llmprovider',
-    #         name='type',
-    #         field=models.CharField(choices=[
-    #             ('openai', 'OpenAI'),
-    #             ('azure', 'Azure OpenAI'),
-    #             ('anthropic', 'Anthropic'),
-    #             ('groq', 'Groq'),
-    #             ('perplexity', 'Perplexity'),
-    #             ('deepseek', 'DeepSeek'),
-    #             ('google', 'Google Gemini')
-    #         ], max_length=255),
-    #     ),
-    #     migrations.AlterField(
-    #         model_name='llmprovidermodel',
-    #         name='type',
-    #         field=models.CharField(choices=[
-    #             ('openai', 'OpenAI'),
-    #             ('azure', 'Azure OpenAI'),
-    #             ('anthropic', 'Anthropic'),
-    #             ('groq', 'Groq'),
-    #             ('perplexity', 'Perplexity'),
-    #             ('deepseek', 'DeepSeek'),
-    #             ('google', 'Google Gemini')
-    #         ], max_length=255),
-    #     ),
-    # ]
