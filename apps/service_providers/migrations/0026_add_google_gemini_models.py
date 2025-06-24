fabe73bdd2f9a568fa243aafb9dab5eccdb66973
barora05 <barora@dimagi-associate.com>
2025-06-24 16:04:59 +0530

    add alter field operation

--- apps/service_providers/migrations/0026_add_google_gemini_models.py
+++ apps/service_providers/migrations/0026_add_google_gemini_models.py
@@ -18,3 +18,31 @@ class Migration(migrations.Migration):
     dependencies = [
         ('service_providers', '0025_alter_llmprovider_type_alter_llmprovidermodel_type'),
     ]
+    operations = [
+        migrations.AlterField(
+            model_name='llmprovider',
+            name='type',
+            field=models.CharField(choices=[
+                ('openai', 'OpenAI'),
+                ('azure', 'Azure OpenAI'),
+                ('anthropic', 'Anthropic'),
+                ('groq', 'Groq'),
+                ('perplexity', 'Perplexity'),
+                ('deepseek', 'DeepSeek'),
+                ('google', 'Google Gemini')
+            ], max_length=255),
+        ),
+        migrations.AlterField(
+            model_name='llmprovidermodel',
+            name='type',
+            field=models.CharField(choices=[
+                ('openai', 'OpenAI'),
+                ('azure', 'Azure OpenAI'),
+                ('anthropic', 'Anthropic'),
+                ('groq', 'Groq'),
+                ('perplexity', 'Perplexity'),
+                ('deepseek', 'DeepSeek'),
+                ('google', 'Google Gemini')
+            ], max_length=255),
+        ),
+    ]
