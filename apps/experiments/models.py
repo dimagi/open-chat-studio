2c654e6d2bd8ac28ac798d5a937f021a82200404
Simon Kelly <skelly@dimagi.com>
2025-06-27 15:40:30 +0200

    make fields properties

--- apps/experiments/models.py
+++ apps/experiments/models.py
@@ -1643,6 +1643,7 @@ class ExperimentSession(BaseTeamModel):
             return False
         return self.experiment_channel.experiment != self.experiment
 
+    @property
     def is_complete(self):
         return self.status == SessionStatus.COMPLETE
 
