112dd4951b8f50720c61bbb5c871290192318269
Farid Rener <proteusvacuum@gmail.com>
2025-07-02 11:26:14 -0400

    Use all participants if there is no experiment

--- apps/participants/views.py
+++ apps/participants/views.py
@@ -168,10 +168,17 @@ def cancel_schedule(request, team_slug: str, participant_id: int, schedule_id: s
 
 @permission_required("experiments.view_participant")
 @login_and_team_required
-def participant_identifiers(request, team_slug: str, experiment_id: int):
+def participant_identifiers_by_experiment(request, team_slug: str, experiment_id: int):
     identifiers = list(
         Participant.objects.filter(team=team_slug, experimentsession__experiment_id=experiment_id)
         .values_list("identifier", flat=True)
         .distinct()
     )
+    return JsonResponse(identifiers, safe=False)
+
+
+@permission_required("experiments.view_participant")
+@login_and_team_required
+def all_participant_identifiers(request, team_slug: str):
+    identifiers = list(Participant.objects.filter(team=team_slug).values_list("identifier", flat=True).distinct())
     return JsonResponse(identifiers, safe=False)
