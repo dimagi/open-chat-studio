11e63477014cd58450b62e7ada978e6c8b849302
Simon Kelly <skelly@dimagi.com>
2025-07-03 17:07:57 +0200

    revert dashboard changes

--- apps/dashboard/services.py
+++ apps/dashboard/services.py
@@ -52,10 +52,9 @@ class DashboardService:
             start_date = end_date - timedelta(days=30)
 
         base_filters = {"created_at__gte": start_date, "created_at__lte": end_date}
-        print(base_filters, experiment_ids, platform_names)
 
         # Base querysets
-        experiments = Experiment.objects.filter(team=self.team, is_archived=False, working_version=None)
+        experiments = Experiment.objects.filter(team=self.team, is_archived=False)
         sessions = ExperimentSession.objects.filter(team=self.team, **base_filters)
         messages = ChatMessage.objects.filter(chat__team=self.team, **base_filters)
         participants = Participant.objects.filter(team=self.team)
