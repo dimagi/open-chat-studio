a1204c6a29c1a31aa1bc3b1e06c8fbb64fa36fb7
Simon Kelly <skelly@dimagi.com>
2025-08-07 15:29:51 +0200

    config model

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -8,13 +8,3 @@ import './tom-select';
 import './theme-toggle';
 import './tables'
 import 'open-chat-studio-widget';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const element = document.querySelector('open-chat-studio-widget ');
-  if (element) {
-    let welcomeMessages = ['Hi! Welcome to our support chat.', 'How can we help you today?'];
-    let starterQuestions = ['How do I create a bot?', 'How do I connect my bot to WhatsApp?']
-    element.welcomeMessages = JSON.stringify(welcomeMessages)
-    element.starterQuestions = JSON.stringify(starterQuestions)
-  }
-})
