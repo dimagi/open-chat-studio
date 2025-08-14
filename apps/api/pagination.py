c35f3893f0adf75e7db3274bdc04337879ffdfea
Simon Kelly <skelly@dimagi.com>
2025-08-14 16:46:09 +0200

    allow configuration of the page size

--- apps/api/pagination.py
+++ apps/api/pagination.py
@@ -3,3 +3,4 @@ from rest_framework.pagination import CursorPagination as RestCursorPagination
 
 class CursorPagination(RestCursorPagination):
     ordering = "-created_at"
+    page_size_query_param = "page_size"
