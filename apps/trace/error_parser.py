b6c114446f0ca9e271065888db68d371d47ee3b2
Chris Smit <chris.smit@dimagi.com>
2025-08-14 13:31:10 +0200

    Don't use too granular span tags

--- apps/trace/error_parser.py
+++ apps/trace/error_parser.py
@@ -2,15 +2,11 @@
 Errors can be categorized or tagged based on where or why it occurs.
 """
 
-import re
-
 from django.db import models
-from openai import OpenAIError
+from openai import BadRequestError, OpenAIError
 
 from apps.chat.exceptions import AudioSynthesizeException
 from apps.pipelines.exceptions import CodeNodeRunError
-
-FLOAT_PATTERN = r"[+-]?([0-9]*[.])?[0-9]+"  # matches floating point numbers
 
 
 class ErrorCategory(models.TextChoices):
@@ -18,9 +14,8 @@ class ErrorCategory(models.TextChoices):
     RATE_LIMIT = "rate limit"
     AUTH = "auth"
     CODE_NODE = "code node"
-    EMPTY_USER_MESSAGE = "empty user message"
-    UNSUPPORTED_TEMPERATURE = "unsupported temperature"
-    SYNTHESIZE_AUDIO = "synthesize audio"
+    BAD_API_CALL = "bad api call"
+    AUDIO_SYNTHESIS = "audio synthesis"
     UNKNOWN = "unknown"
 
 
@@ -31,7 +26,7 @@ def get_tags_from_error(error: Exception):
     elif isinstance(error, CodeNodeRunError):
         tags.append(ErrorCategory.CODE_NODE)
     elif isinstance(error, AudioSynthesizeException):
-        tags.append(ErrorCategory.SYNTHESIZE_AUDIO)
+        tags.append(ErrorCategory.AUDIO_SYNTHESIS)
     else:
         tags.append(ErrorCategory.UNKNOWN)
     return tags
@@ -40,8 +35,6 @@ def get_tags_from_error(error: Exception):
 def _parse_openai_error(error: OpenAIError) -> ErrorCategory:
     if "Incorrect API key provided" in error.message:
         return ErrorCategory.AUTH
-    elif "Message content must be non-empty" in error.message:
-        return ErrorCategory.EMPTY_USER_MESSAGE
-    elif re.match(rf".*'temperature' does not support {FLOAT_PATTERN} with this model", error.message):
-        return ErrorCategory.UNSUPPORTED_TEMPERATURE
+    elif isinstance(error, BadRequestError):
+        return ErrorCategory.BAD_API_CALL
     return ErrorCategory.UNKNOWN
