71a103a89f506193c9ce6df2061c1ecb8720df8e
Steph Herbers <stephherbers@gmail.com>
2025-09-02 09:07:29 -0400

    modify channel models

--- apps/channels/models.py
+++ apps/channels/models.py
@@ -32,6 +32,7 @@ class ChannelPlatform(models.TextChoices):
     SLACK = "slack", "Slack"
     COMMCARE_CONNECT = "commcare_connect", "CommCare Connect"
     EVALUATIONS = "evaluations", "Evaluations"
+    EMBEDDED_WIDGET = "embedded_widget", "Embedded Widget"
 
     @classmethod
     def team_global_platforms(cls):
@@ -46,6 +47,7 @@ class ChannelPlatform(models.TextChoices):
         all_platforms = cls.as_list(exclude=[cls.API, cls.WEB, cls.EVALUATIONS])
         platform_availability = {platform: False for platform in all_platforms}
         platform_availability[cls.TELEGRAM] = True
+        platform_availability[cls.EMBEDDED_WIDGET] = True
 
         for provider in MessagingProvider.objects.filter(team=team):
             for platform in provider.get_messaging_service().supported_platforms:
@@ -91,6 +93,9 @@ class ChannelPlatform(models.TextChoices):
             case self.COMMCARE_CONNECT:
                 kwargs.pop("channel", None)
                 return forms.CommCareConnectChannelForm(*args, **kwargs)
+            case self.EMBEDDED_WIDGET:
+                kwargs.pop("channel", None)
+                return forms.EmbeddedWidgetChannelForm(*args, **kwargs)
         return None
 
     @property
@@ -110,6 +115,8 @@ class ChannelPlatform(models.TextChoices):
                 # The bot_name will be shown to the user, which is how they'll know which bot it is. We use the bot name
                 # here to prevent other bots from using the same name in order to mitigate confusion.
                 return "commcare_connect_bot_name"
+            case self.EMBEDDED_WIDGET:
+                return "widget_token"
         return None
 
     @staticmethod
