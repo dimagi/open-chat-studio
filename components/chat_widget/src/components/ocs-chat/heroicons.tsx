23c3c258731f5769af23123859092c8412fc402c
barora05 <barora@dimagi-associate.com>
2025-09-10 13:15:13 +0530

    remove extra icons

--- components/chat_widget/src/components/ocs-chat/heroicons.tsx
+++ components/chat_widget/src/components/ocs-chat/heroicons.tsx
@@ -76,19 +76,3 @@ export const XIcon = () => {
     </svg>
   )
 }
-
-export const ArrowUpTrayIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-    </svg>
-  )
-}
-
-export const ChevronDownIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-    </svg>
-  )
-}
