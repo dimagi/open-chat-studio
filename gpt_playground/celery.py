6dc2f208fb9e27a1653988200e22f8082d8d9e53
Simon Kelly <skelly@dimagi.com>
2025-09-16 15:15:39 +0200

    log DB connection pool stats in Celery

--- gpt_playground/celery.py
+++ gpt_playground/celery.py
@@ -1,7 +1,10 @@
+import logging
 import os
 
 from celery import Celery
 from celery.app import trace
+from celery.signals import task_prerun
+from django.db import connections
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gpt_playground.settings")
@@ -22,3 +25,11 @@ app.conf.result_expires = 86400  # expire results in redis in 1 day
 trace.LOG_SUCCESS = """\
 Task %(name)s[%(id)s] succeeded in %(runtime)ss\
 """
+
+logger = logging.getLogger("ocs.debug")
+
+
+@task_prerun.connect
+def log_pool_stats(*args, **kwargs):
+    for conn in connections.all():
+        logger.info(conn.pool.get_stats())
