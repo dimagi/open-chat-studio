596cec3e6bae4024ff24cb0b8d58c758c67edd58
Steph Herbers <stephherbers@gmail.com>
2025-09-17 15:11:55 -0400

    add is_super_admin utils check

--- apps/teams/roles.py
+++ apps/teams/roles.py
@@ -1,4 +1,5 @@
 import apps.teams.models
+from apps.teams.backends import SUPER_ADMIN_GROUP
 from apps.users.models import CustomUser
 
 
@@ -6,3 +7,10 @@ def is_member(user: CustomUser, team: apps.teams.models.Team) -> bool:
     if not team:
         return False
     return team.members.filter(id=user.id).exists()
+
+
+def is_super_admin(user: CustomUser, team: apps.teams.models.Team) -> bool:
+    if not user or not user.is_authenticated or not team:
+        return False
+    membership = user.membership_set.get(team=team)
+    return membership.groups.filter(name=SUPER_ADMIN_GROUP).exists()
