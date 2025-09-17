270745701aaa81861277192ce6851a2e20b9b78a
barora05 <barora@dimagi-associate.com>
2025-09-17 20:23:26 +0530

    return default translations

--- components/chat_widget/src/components/ocs-chat/ocs-chat.tsx
+++ components/chat_widget/src/components/ocs-chat/ocs-chat.tsx
@@ -8,7 +8,7 @@ import {
 import { renderMarkdownSync as renderMarkdownComplete } from '../../utils/markdown';
 import { getCSRFToken } from '../../utils/cookies';
 import { varToPixels } from '../../utils/utils';
-import {TranslationStrings, TranslationManager, getBrowserLanguage} from '../../utils/translations';
+import {TranslationStrings, TranslationManager, getBrowserLanguage, defaultTranslations} from '../../utils/translations';
 
 interface ChatMessage {
   created_at: string;
@@ -344,7 +344,7 @@ export class OcsChat {
       return translations as Partial<TranslationStrings>;
     } catch (error) {
       console.error('Error loading translations from URL:', error);
-      throw error;
+      return defaultTranslations
     }
   }
 
