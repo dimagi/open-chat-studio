3cb5e911091955ffbfb68376708b2775a488081a
Chris Smit <chris.smit@dimagi.com>
2025-09-18 08:50:32 +0200

    Remove duplicate json converter filter

--- apps/web/templatetags/json_tags.py
+++ apps/web/templatetags/json_tags.py
@@ -19,21 +19,4 @@ def to_json(obj):
     # json.dumps does not properly convert QueryDict array parameter to json
     if isinstance(obj, QueryDict):
         obj = dict(obj)
-    return mark_safe(escape_script_tags(json.dumps(obj, cls=DjangoJSONEncoder)))
-
-
-@register.filter
-def prettyjson(obj):
-    """Convert a Python object to pretty-formatted JSON string."""
-    if obj is None:
-        return "{}"
-
-    # json.dumps does not properly convert QueryDict array parameter to json
-    if isinstance(obj, QueryDict):
-        obj = dict(obj)
-
-    try:
-        return json.dumps(obj, cls=DjangoJSONEncoder, indent=2, ensure_ascii=False)
-    except (TypeError, ValueError):
-        # If object is not JSON serializable, return string representation
-        return str(obj)
+    return mark_safe(escape_script_tags(json.dumps(obj, indent=2, cls=DjangoJSONEncoder)))
