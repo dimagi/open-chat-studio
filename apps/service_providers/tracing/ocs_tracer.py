0e9230fa0ec7377f322189117429f2c79d20853e
Chris Smit <chris.smit@dimagi.com>
2025-09-22 08:01:34 +0200

    Remove already implemented TODO comment

--- apps/service_providers/tracing/ocs_tracer.py
+++ apps/service_providers/tracing/ocs_tracer.py
@@ -22,7 +22,6 @@ if TYPE_CHECKING:
 logger = logging.getLogger("ocs.tracing")
 
 
-# TODO in followup PR: Return link to trace in get_trace_metadata
 class OCSTracer(Tracer):
     """
     Internal OCS tracer that creates Trace objects in the database.
