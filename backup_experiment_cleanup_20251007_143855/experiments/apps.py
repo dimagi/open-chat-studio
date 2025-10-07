9c6069b8b0b0711954022d4915896946b64c76b8
Steph Herbers <stephherbers@gmail.com>
2025-10-07 15:44:15 -0400

    Remove backup files created during experiment cleanup
    - Clean up backup directory that was created for safety
    - Repository now contains only the cleaned up code

--- backup_experiment_cleanup_20251007_143855/experiments/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExperimentAppConfig(AppConfig):
-    name = "apps.experiments"
-    label = "experiments"
-
-    def ready(self):
-        from . import signals  # noqa  F401
