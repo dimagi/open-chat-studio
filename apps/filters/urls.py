15f4eacbf11d82382182636fe21bb943cf73f55e
Chris Smit <chris.smit@dimagi.com>
2025-10-16 15:38:38 +0200

    Remove duplicate filter path

--- apps/filters/urls.py
+++ apps/filters/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
         name="filter_set",
     ),
     path(
-        "filters/<str:table_type>/list/",
+        "<str:table_type>/list/",
         views.list_filter_sets,
         name="list_filter_set",
     ),
     path(
-        "filters/<str:table_type>/create/",
+        "<str:table_type>/create/",
         views.create_filter_set,
         name="create_filter_set",
     ),
