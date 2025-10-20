b7f68e3f2b0f64e5d2e6a79d5a3b967ff31f6024
Simon Kelly <skelly@dimagi.com>
2025-10-20 16:35:15 +0200

    add django-watchfiles

--- gpt_playground/settings.py
+++ gpt_playground/settings.py
@@ -64,6 +64,7 @@ THIRD_PARTY_APPS = [
     "django_otp",
     "django_otp.plugins.otp_totp",
     "django_otp.plugins.otp_static",
+    "django_watchfiles",
     "allauth_2fa",
     "rest_framework",
     "drf_spectacular",
