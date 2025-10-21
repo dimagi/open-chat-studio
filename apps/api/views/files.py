6fba325c88b629a8ab5b68d44a0ef0940434af77
Simon Kelly <skelly@dimagi.com>
2025-10-21 12:26:33 +0200

    remove json renderer and update format name

--- apps/api/views/files.py
+++ apps/api/views/files.py
@@ -3,14 +3,14 @@ from django.http import FileResponse, Http404
 from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import BaseRenderer, JSONRenderer
+from rest_framework.renderers import BaseRenderer
 
 from apps.files.models import File
 
 
 class BinaryRenderer(BaseRenderer):
     media_type = "application/octet-stream"
-    format = "bin"
+    format = "binary"
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
         # For binary content (file downloads), return data as-is
@@ -20,7 +20,7 @@ class BinaryRenderer(BaseRenderer):
 
 @extend_schema(operation_id="file_content", summary="Download File Content", tags=["Files"], responses=bytes)
 @api_view(["GET"])
-@renderer_classes([BinaryRenderer, JSONRenderer])
+@renderer_classes([BinaryRenderer])
 @permission_required("files.view_file")
 def file_content_view(request, pk: int):
     file = get_object_or_404(File, id=pk, team=request.team)
