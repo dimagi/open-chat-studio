a1df53ca2aa04edf3eb563a5f98de035273b5a8d
Chris Smit <chris.smit@dimagi.com>
2025-10-23 14:05:04 +0200

    Enable SECURE_HSTS_SECONDS

--- gpt_playground/settings_production.py
+++ gpt_playground/settings_production.py
@@ -16,10 +16,10 @@ CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/3.2/ref/middleware/#http-strict-transport-security
 
 # # Increase this number once you're confident everything works https://stackoverflow.com/a/49168623/8207
-# SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_SECONDS = 60
 # # Uncomment these two lines if you are sure that you don't host any subdomains over HTTP.
 # # You will get security warnings if you don't do this.
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 # SECURE_HSTS_PRELOAD = True
 
 USE_HTTPS_IN_ABSOLUTE_URLS = True
