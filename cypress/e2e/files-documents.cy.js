a53cf1a8cb75117cfd20753b4c22ebc8ab03a47c
Steph Herbers <stephherbers@gmail.com>
2025-10-27 20:04:18 -0400

    update file/document tests

--- cypress/e2e/files-documents.cy.js
+++ cypress/e2e/files-documents.cy.js
@@ -1,192 +1,212 @@
-describe('Files and collections Application', () => {
+describe('Files and Collections Application', () => {
   const teamSlug = Cypress.env('TEAM_SLUG') || 'your-team-slug'
 
-  beforeEach(() => {
+  before(() => {
     cy.login()
   })
 
   describe('Files Home Page', () => {
+    beforeEach(() => {
+      cy.login()
+      cy.visit(`/a/${teamSlug}/files/file`, { failOnStatusCode: false })
+      cy.get('body').should('be.visible')
+      cy.wait(500) // Wait for HTMX
+    })
+
     it('loads files page successfully', () => {
-      cy.visit(`/a/${teamSlug}/files/file`)
       cy.url().should('include', '/files/')
-      cy.contains('Files').should('be.visible')
     })
 
     it('displays files table', () => {
-      cy.visit(`/a/${teamSlug}/files/file`)
       cy.get('table, .table-container, [data-table]', { timeout: 10000 }).should('exist')
     })
 
     it('has search functionality', () => {
-      cy.visit(`/a/${teamSlug}/files/file`)
-      cy.get('input[type="search"], input[name="search"]').then(($search) => {
-        if ($search.length > 0) {
-          cy.wrap($search).type('test{enter}')
-          cy.wait(500)
-        }
-      })
+      cy.get('input[type="search"], input[name="search"]', { timeout: 10000 }).should('exist')
+      cy.get('input[type="search"], input[name="search"]').type('test')
+      cy.wait(500)
     })
 
     it('displays file information in table', () => {
-      cy.visit(`/a/${teamSlug}/files/file`)
-      cy.get('table tbody tr').first().then(($row) => {
-        if ($row.length > 0) {
-          cy.log('File table row exists')
-        }
-      })
+      cy.get('table tbody tr', { timeout: 1000 }).should('exist')
+      cy.get('table tbody tr').should('have.length.greaterThan', 0)
     })
   })
-  describe('Edit File', () => {
+
+  describe('File Details and Edit', () => {
+    beforeEach(() => {
+      cy.login()
+      cy.visit(`/a/${teamSlug}/files/file`, { failOnStatusCode: false })
+      cy.get('body').should('be.visible')
+      cy.wait(500)
+    })
+
+    it('can navigate to file details', () => {
+      cy.get('table tbody tr', { timeout: 1000 }).should('exist')
+      cy.get('table tbody tr a').first().click({force: true})
+      cy.url().should('include', '/files/')
+      cy.get('body').should('be.visible')
+    })
+
     it('can access edit file page', () => {
-      cy.visit(`/a/${teamSlug}/files/file`)
-      cy.get('table tbody tr a').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.contains('button, a', /Edit/i).then(($edit) => {
-            if ($edit.length > 0) {
-              cy.wrap($edit).click()
-              cy.get('form').should('exist')
-            }
-          })
-        }
-      })
+      cy.get('table tbody tr', { timeout: 1000 }).should('exist')
+      cy.get('table tbody tr a').first().click({force: true})
+      cy.wait(500)
+      cy.contains('button, a', /Edit/i, { timeout: 10000 }).should('exist').click({force: true})
+      cy.get('form').should('exist')
+      cy.contains('h1', /Edit File/i).should('exist')
     })
 
-    it('edit form shows current file info', () => {
-      cy.visit(`/a/${teamSlug}/files/file`)
-      cy.get('table tbody tr a').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.contains('button, a', /Edit/i).then(($edit) => {
-            if ($edit.length > 0) {
-              cy.wrap($edit).click()
-              cy.get('input[name="name"]').should('exist')
-            }
-          })
-        }
-      })
+    it('edit form shows file name field', () => {
+      cy.get('table tbody tr', { timeout: 10000 }).should('exist')
+      cy.get('table tbody tr a').first().click({force: true})
+      cy.wait(500)
+      cy.contains('button, a', /Edit/i, { timeout: 10000 }).click({force: true})
+      cy.get('input#id_name[name="name"]').should('exist')
+      cy.get('input#id_name').should('have.attr', 'required')
+    })
+
+    it('edit form has summary field', () => {
+      cy.get('table tbody tr', { timeout: 10000 }).should('exist')
+      cy.get('table tbody tr a').first().click({force: true})
+      cy.wait(500)
+      cy.contains('button, a', /Edit/i, { timeout: 10000 }).click({force: true})
+      cy.get('textarea#id_summary[name="summary"]').should('exist')
+    })
+
+    it('edit form has update button', () => {
+      cy.get('table tbody tr', { timeout: 10000 }).should('exist')
+      cy.get('table tbody tr a').first().click({force: true})
+      cy.wait(500)
+      cy.contains('button, a', /Edit/i, { timeout: 10000 }).click({force: true})
+      cy.get('input[type="submit"]').should('exist')
+      cy.get('input[type="submit"]').should('have.value', 'Update')
+    })
+
+    it('edit form displays collections', () => {
+      cy.get('table tbody tr', { timeout: 10000 }).should('exist')
+      cy.get('table tbody tr a').first().click({force: true})
+      cy.wait(500)
+      cy.contains('button, a', /Edit/i, { timeout: 10000 }).click({force: true})
+      cy.contains('h3', /Collections/i).should('exist')
     })
   })
 
-  describe('collections Home Page', () => {
-    it('loads collections page successfully', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.url().should('include', '/collection/')
+  describe('Collections Home Page', () => {
+    beforeEach(() => {
+      cy.login()
+      cy.visit(`/a/${teamSlug}/documents/collection/`, { failOnStatusCode: false })
+      cy.get('body').should('be.visible')
+      cy.wait(500)
     })
 
-    it('displays document collections', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.contains(/collections|collections/i).should('exist')
+    it('loads collections page successfully', () => {
+      cy.url().should('include', '/collection/')
+      cy.get('h1, h2, h3').should('exist')
+    })
+
+    it('displays collections content', () => {
+      // Page should have some content about collections
+      cy.get('body').should('not.be.empty')
     })
 
     it('has create collection button', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.contains('button, a', /Add new/i).should('exist')
+      cy.contains('button, a', /Add New/i, { timeout: 10000 }).should('exist')
     })
   })
+
+  // Helper to open the first collection link
+  const openFirstCollection = () => {
+    cy.get('tbody tr').first().click()
+    cy.url().should('match', /\/documents\/collections\/\d+/)
+  }
 
   describe('Collection Details', () => {
+    beforeEach(() => {
+      cy.login()
+      cy.visit(`/a/${teamSlug}/documents/collection/`, { failOnStatusCode: false })
+      cy.get('body').should('be.visible')
+      cy.wait(200)
+    })
+
     it('navigates to collection details', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.get('a[href*="/collections/"]').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.url().should('match', /documents\/collection\/\d+/)
-        }
-      })
+      openFirstCollection()
     })
 
+    it('collection detail page has title and content', () => {
+      openFirstCollection()
+      cy.get('h1.pg-title', { timeout: 10000 }).should('exist')
+    })
 
-    it('shows add files button', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.get('a[href*="/documents/collection/"]').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.contains('button, a', /Add Files/i).then(($add) => {
-            if ($add.length > 0) {
-              cy.log('Add files functionality available')
-            }
-          })
-        }
-      })
+    it('collection shows file count information', () => {
+      openFirstCollection()
+      cy.contains(/files remaining|files uploaded|remaining/i, { timeout: 10000 }).should('exist')
     })
   })
 
-  describe('Add Files to Collection', () => {
-    it('can upload files to collection', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.get('a[href*="/documents/collection/"]').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.contains('button, a', /Add Files/i).then(($upload) => {
-            if ($upload.length > 0) {
-              cy.wrap($upload).click()
-              cy.get('input[type="file"], form').should('exist')
-            }
-          })
-        }
-      })
+  describe('Collection Actions', () => {
+    beforeEach(() => {
+      cy.login()
+      cy.visit(`/a/${teamSlug}/documents/collection/`, { failOnStatusCode: false })
+      cy.get('body').should('be.visible')
+      cy.wait(200)
+    })
+
+    it('collection has search functionality', () => {
+      openFirstCollection()
+      cy.get('input#search-input[type="search"]').should('exist')
+      cy.get('input#search-input').should('have.attr', 'placeholder', 'Search files...')
+    })
+
+    it('collection has file upload modal', () => {
+      openFirstCollection()
+      cy.get('dialog#chooseFilesModal', { timeout: 10000 }).should('exist')
     })
   })
 
-  describe('Collection Processing', () => {
-    it('shows processing status', () => {
-      cy.visit(`/a/${teamSlug}/collections/`)
-      cy.get('a[href*="/collections/"]').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.contains(/Status|Processing|Ready/i).then(($status) => {
-            if ($status.length > 0) {
-              cy.log('Collection status displayed')
-            }
-          })
-        }
+  describe('Collection Management', () => {
+    beforeEach(() => {
+      cy.login()
+      cy.visit(`/a/${teamSlug}/documents/collection/`, { failOnStatusCode: false })
+      cy.get('body').should('be.visible')
+      cy.wait(200)
+    })
+    it('collection page has breadcrumb navigation', () => {
+      openFirstCollection()
+      cy.get('.breadcrumbs', { timeout: 10000 }).should('exist')
+      cy.get('.breadcrumbs').within(() => {
+        cy.contains('Collections').should('exist')
       })
     })
 
-    it('has refresh/sync button', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.get('a[href*="/documents/collection/"]').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.contains('button', /Refresh|Sync|Process/i).then(($sync) => {
-            if ($sync.length > 0) {
-              cy.log('Refresh/sync functionality available')
-            }
-          })
-        }
-      })
+    it('collection displays files section', () => {
+      openFirstCollection()
+      cy.contains('h2', /Files/i).should('exist')
+      cy.get('#collection-files-container').should('exist')
+    })
+
+    it('collection has query/search link', () => {
+      openFirstCollection()
+      cy.get('select, input[type="search"]', { timeout: 1000 }).should('exist')
     })
   })
 
-  describe('Delete Collection', () => {
-    it('has delete collection button', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.get('a[href*="/documents/collection/"]').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.contains('button, a', /Delete|Remove/i).then(($delete) => {
-            if ($delete.length > 0) {
-              cy.log('Delete collection functionality available')
-            }
-          })
-        }
-      })
+  describe('File Details and Edit', () => {
+    beforeEach(() => {
+      cy.login()
+      cy.visit(`/a/${teamSlug}/documents/collection/`, { failOnStatusCode: false })
+      cy.get('body').should('be.visible')
+      cy.wait(200)
     })
 
-    it('delete requires confirmation', () => {
-      cy.visit(`/a/${teamSlug}/documents/collection/`)
-      cy.get('a[href*="/documents/collection/"]').first().then(($link) => {
-        if ($link.length > 0) {
-          cy.wrap($link).click()
-          cy.contains('button, a', /Delete/i).then(($delete) => {
-            if ($delete.length > 0) {
-              cy.wrap($delete).click()
-              cy.contains(/confirm|warning|sure/i, { timeout: 3000 }).should('exist')
-            }
-          })
-        }
-      })
+    it('can navigate to file details', () => {
+      openFirstCollection()
+      cy.wait(400)
+      cy.get('#collection-files-container')
+        .find('a.btn.btn-sm.btn-soft.btn-primary')
+        .first()
+        .click({ force: true })
+      cy.url().should('match', /\/files\/file\/\d+/)
     })
   })
 })
