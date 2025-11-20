d908ea8a9a84b7bdcd431beab72e720863583a9a
Simon Kelly <skelly@dimagi.com>
2025-11-20 14:19:46 +0200

    update all nodes, not only those with the supported models

--- apps/service_providers/migration_utils.py
+++ apps/service_providers/migration_utils.py
@@ -1,7 +1,5 @@
 from django.db import migrations
 
-from apps.service_providers.llm_service.default_models import LLM_MODEL_PARAMETERS
-
 
 def populate_temperature_params(Node, LlmProviderModel):
     """
@@ -13,36 +11,26 @@ def populate_temperature_params(Node, LlmProviderModel):
     For now we're not going to remove the existing `llm_temperature` toplevel parameter, just to be safe.
     It should be OK to leave it there indefinitely, since we wouldn't be using it anymore.
     """
-
-    models_supporting_temperature = []
-    for model, param_cls in LLM_MODEL_PARAMETERS.items():
-        if "temperature" in param_cls.model_fields:
-            models_supporting_temperature.append(model)
-
-    supported_model_ids = list(
-        LlmProviderModel.objects.filter(name__in=models_supporting_temperature).values_list("id", flat=True)
-    )
-
     nodes_to_save = []
-
-    # Some ids are saved as strings, others as ints, so we need to check for both
-    int_ids = [id for id in supported_model_ids]
-    str_ids = [str(id) for id in supported_model_ids]
-    for node in Node.objects.filter(params__llm_provider_model_id__in=int_ids + str_ids).iterator():
+    node_types = ["LLMResponseWithPrompt", "RouterNode", "ExtractParticipantData", "ExtractStructuredData"]
+    for node in Node.objects.filter(type__in=node_types).iterator():
         params = node.params
-        llm_model_params = params.get("llm_model_parameters") or {}
-
-        # llm_temperature should exist for all these models, but default to 0.7 just in case
         temp = params.get("llm_temperature")
+        temp = temp if temp is not None else 0.7
+        try:
+            temp = float(temp)
+        except (ValueError, TypeError):
+            temp = 0.7
+        original_llm_model_params = params.get("llm_model_parameters")
+        llm_model_params = original_llm_model_params.copy() if original_llm_model_params else {}
         llm_model_params["temperature"] = temp if temp is not None else 0.7
-        params["llm_model_parameters"] = llm_model_params
-        nodes_to_save.append(node)
-
+        if llm_model_params != original_llm_model_params:
+            params["llm_model_parameters"] = llm_model_params
+            nodes_to_save.append(node)
         if len(nodes_to_save) >= 100:
             Node.objects.bulk_update(nodes_to_save, ["params"])
             nodes_to_save = []
 
-    # Final save for any remaining nodes
     if nodes_to_save:
         Node.objects.bulk_update(nodes_to_save, ["params"])
 
