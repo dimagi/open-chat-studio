0edea511e47594a7994268179001847d71f7dd93
Chris Smit <chris.smit@dimagi.com>
2025-11-21 10:23:32 +0200

    Implement the other abstract models from the oauth2_provider library as well and link the team table to the access token and grant models
    - This is required if we want to implement anything except the AbstractApplication model.
    - We decided to link the team to the model (so access tokens are auto scoped to teams) and rather reserve the scopes for APIs

--- apps/oauth/models.py
+++ apps/oauth/models.py
@@ -1,6 +1,39 @@
-from oauth2_provider.models import AbstractApplication, ApplicationManager
+"""
+Custom OAuth2 models to extend the default django-oauth-toolkit models.
+
+To avoid a myriad of migration issues, we have to implement all abstract models.
+Related thread: https://github.com/django-oauth/django-oauth-toolkit/issues/634
+"""
+
+from django.db import models
+from oauth2_provider.models import (
+    AbstractAccessToken,
+    AbstractApplication,
+    AbstractGrant,
+    AbstractIDToken,
+    AbstractRefreshToken,
+    ApplicationManager,
+)
+
+from apps.teams.models import Team
 
 
 class OAuth2Application(AbstractApplication):
     # Custom application model can be extended here if needed
     objects = ApplicationManager()
+
+
+class OAuth2AccessToken(AbstractAccessToken):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class OAuth2Grant(AbstractGrant):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class OAuth2IDToken(AbstractIDToken):
+    pass
+
+
+class OAuth2RefreshToken(AbstractRefreshToken):
+    pass
