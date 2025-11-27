9e7225035bc27c7c94372a9ab2d6d3157d24785e
Simon Kelly <skelly@dimagi.com>
2025-11-27 08:13:21 +0200

    depend on race migration

--- apps/experiments/migrations/0120_backfill_session_fields.py
+++ apps/experiments/migrations/0120_backfill_session_fields.py
@@ -7,6 +7,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('experiments', '0119_experimentsession_experiment_versions_and_more'),
+        ('trace', '0006_alter_span_input_alter_span_metadata_and_more'),
     ]
 
     operations = [
