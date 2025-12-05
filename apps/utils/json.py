859e9ba46b77e3da762db2cfd145c5e73e5f81ac
Simon Kelly <skelly@dimagi.com>
2025-12-05 20:50:10 +0200

    update pretty encoder

--- apps/utils/json.py
+++ apps/utils/json.py
@@ -2,5 +2,8 @@ import json
 
 
 class PrettyJSONEncoder(json.JSONEncoder):
-    def __init__(self, *args, indent, sort_keys, **kwargs):
-        super().__init__(*args, indent=4, sort_keys=True, **kwargs)
+    def __init__(self, *args, **kwargs):
+        # Force pretty-printing regardless of any caller-provided values.
+        kwargs["indent"] = 4
+        kwargs["sort_keys"] = True
+        super().__init__(*args, **kwargs)
