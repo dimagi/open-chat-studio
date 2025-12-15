7ee19aea04e8dd1da9861925784ce1c9f1ca98f5
Diego Tolentino <dtolentino@dimagi-associate.com>
2025-12-15 12:25:47 -0300

    #1208 Ajust sentry suggestion

--- apps/annotations/models.py
+++ apps/annotations/models.py
@@ -100,7 +100,7 @@ class TaggedModelMixin(models.Model, AnnotationMixin):
         abstract = True
 
     tags = TaggableManager(through=CustomTaggedItem)
-    _skipped_category_tags: ClassVar[list] = [TagCategories.VOICE]  # Tag categories with special treatment
+    _skipped_category_tags: ClassVar[list] = [TagCategories.VOICE.value]  # Tag categories with special treatment
 
     def add_tags(self, tags: list[str], team: Team, added_by: CustomUser = None):
         tag_objs = Tag.objects.filter(team=team, name__in=tags)
@@ -129,7 +129,7 @@ class TaggedModelMixin(models.Model, AnnotationMixin):
         return [tag["name"] for tag in self.prefetched_tags_json]
 
     def has_voice_tag(self):
-        return any([tag for tag in self.prefetched_tags_json if tag["category"] == TagCategories.VOICE])
+        return any([tag for tag in self.prefetched_tags_json if tag["category"] == TagCategories.VOICE.value])
 
     def non_skipped_tags(self):
         return [tag for tag in self.prefetched_tags_json if tag["category"] not in self._skipped_category_tags]
