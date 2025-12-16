d3d117cdc627906207abd1fea42e6ad50906825b
Chris Smit <chris.smit@dimagi.com>
2025-12-16 08:39:54 +0200

    Safe read fire_end_event and default to true

--- apps/chatbots/views.py
+++ apps/chatbots/views.py
@@ -527,7 +527,7 @@ def end_chatbot_session(request, team_slug: str, experiment_id: uuid.UUID, sessi
         external_id=session_id,
         team=request.team,
     )
-    experiment_session.end(propagate=request.POST["fire_end_event"] == "true")
+    experiment_session.end(propagate=request.POST.get("fire_end_event", "true") == "true")
     messages.success(request, "Session ended")
     return redirect("chatbots:chatbot_session_view", team_slug, experiment_id, session_id)
 
