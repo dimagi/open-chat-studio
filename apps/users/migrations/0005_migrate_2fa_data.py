aee061438ec4413a4aca9fbfa8f37e62e1be0cfb
Simon Kelly <skelly@dimagi.com>
2026-01-09 13:59:44 +0200

    add migration dependency

--- apps/users/migrations/0005_migrate_2fa_data.py
+++ apps/users/migrations/0005_migrate_2fa_data.py
@@ -6,6 +6,7 @@ from apps.data_migrations.utils.migrations import RunDataMigration
 class Migration(migrations.Migration):
     dependencies = [
         ("users", "0004_alter_customuser_managers"),
+        ("mfa", "0001_initial"),
     ]
 
     operations = [
