3e602d988f6ce269fac945e44161d16b4c0972e1
Diego Tolentino <dtolentino@dimagi-associate.com>
2026-01-14 12:27:01 -0300

    #2298 This is the Google's currently recommends it as the default workhorse model for general use

--- apps/service_providers/llm_service/default_models.py
+++ apps/service_providers/llm_service/default_models.py
@@ -102,7 +102,7 @@ DEFAULT_LLM_PROVIDER_MODELS = {
         Model("deepseek-reasoner", 128000, is_translation_default=True),
     ],
     "google": [
-        Model("gemini-2.5-flash", 1048576, deprecated=True, is_default=True),
+        Model("gemini-2.5-flash", 1048576, is_default=True),
         Model("gemini-2.5-pro", 1048576, is_translation_default=True),
         Model("gemini-2.0-flash", 1048576, deprecated=True),
     ],
