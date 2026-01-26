f5986a42950e1e37b8c2ba83850057dbf9bb5ef9
dtolentino-dimagi <dtolentino@dimagi-associate.com>
2026-01-26 16:16:09 -0300

    Removing annotate_with_first_message_created_at from ExperimentSession

--- apps/experiments/models.py
+++ apps/experiments/models.py
@@ -1616,13 +1616,6 @@ class SessionStatus(models.TextChoices):
 
 
 class ExperimentSessionQuerySet(models.QuerySet):
-    def annotate_with_first_message_created_at(self):
-        """Annotate queryset with the created_at timestamp of the first message in each session."""
-        first_message_subquery = Subquery(
-            ChatMessage.objects.filter(chat_id=OuterRef("chat_id")).order_by("created_at").values("created_at")[:1]
-        )
-        return self.annotate(first_message_created_at=first_message_subquery)
-
     def annotate_with_message_count(self):
         message_count_subquery = Subquery(
             ChatMessage.objects
