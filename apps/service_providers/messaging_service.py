dfbb59a40ff594ba182552c2916c029e4269877b
Simon Kelly <skelly@dimagi.com>
2026-01-28 16:07:27 +0200

    use correct type for audio conversion

--- apps/service_providers/messaging_service.py
+++ apps/service_providers/messaging_service.py
@@ -186,7 +186,7 @@ class TwilioService(MessagingService):
         family, sub_type = content_type.split("/", 1)
         if family != "audio":
             raise AudioConversionError(f"Unexpected content-type for audio: {content_type}")
-        return audio.convert_audio(data, target_format="wav", source_format=content_type)
+        return audio.convert_audio(data, target_format="wav", source_format=sub_type)
 
     def _get_account_numbers(self) -> list[str]:
         """Returns all numbers associated with this client account"""
