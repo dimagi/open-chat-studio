fd0230a81f0e66cc53bd4d7e8d4d5e7b33aac7dd
Chris Smit <chris.smit@dimagi.com>
2026-02-10 11:28:23 +0200

    Run data migration as a separate migration before making schema changes

--- apps/experiments/migrations/0127_remove_sessions_with_missing_participants.py
+++ apps/experiments/migrations/0127_remove_sessions_with_missing_participants.py
@@ -15,10 +15,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='experimentsession',
-            name='participant',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='experiments.participant'),
-        ),
         migrations.RunPython(_remove_sessions_with_missing_participants, reverse_code=migrations.RunPython.noop),
     ]
