33ba0936d2c7a725d2b4de9f34cc33a38fbe80d2
Chris Smit <chris.smit@dimagi.com>
2026-03-13 13:26:04 +0200

    Resolve migration number conflicts

--- apps/service_providers/migrations/0045_alter_messagingprovider_type.py
+++ apps/service_providers/migrations/0045_alter_messagingprovider_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('service_providers', '0043_migrate_gemini_3_pro_preview'),
+        ('service_providers', '0044_update_openai_models'),
     ]
 
     operations = [
