9606dec768e29ae6c6dccdc137755818f5fa581e
Simon Kelly <skelly@dimagi.com>
2026-03-16 14:55:10 +0200

    remove django dependency from test

--- apps/pipelines/tests/test_template_node.py
+++ apps/pipelines/tests/test_template_node.py
@@ -4,30 +4,19 @@ from apps.experiments.models import Participant
 from apps.pipelines.exceptions import PipelineNodeRunError
 from apps.pipelines.nodes.base import PipelineState
 from apps.pipelines.nodes.nodes import RenderTemplate
-from apps.pipelines.repository import ORMRepository
+from apps.pipelines.repository import InMemoryPipelineRepository
 from apps.utils.factories.experiment import ExperimentSessionFactory
-from apps.utils.factories.pipelines import PipelineFactory
-
-
-@pytest.fixture()
-def pipeline():
-    return PipelineFactory.create()
-
-
-@pytest.fixture()
-def experiment_session():
-    return ExperimentSessionFactory.create()
 
 
 @pytest.mark.django_db()
-def test_render_template_with_context_keys(pipeline, experiment_session):
-    participant = Participant.objects.create(
+def test_render_template_with_context_keys():
+    experiment_session = ExperimentSessionFactory.build()
+    participant = Participant(
         identifier="participant_123",
         team=experiment_session.team,
         platform="web",
     )
     experiment_session.participant = participant
-    experiment_session.save()
     state = PipelineState(
         experiment_session=experiment_session,
         messages=["Cycling"],
@@ -43,7 +32,7 @@ def test_render_template_with_context_keys(pipeline, experiment_session):
         "input_message_url: {{input_message_url}} "
     )
     node = RenderTemplate(name="test", node_id="123", django_node=None, template_string=template)
-    config = {"configurable": {"repo": ORMRepository(session=experiment_session)}}
+    config = {"configurable": {"repo": InMemoryPipelineRepository(session=experiment_session)}}
     node_output = node.process(incoming_nodes=[], outgoing_nodes=[], state=state, config=config)
     assert node_output["messages"][-1] == (
         "input: Cycling, inputs: ['Cycling'], temp_state.my_key: example_key, "
@@ -53,15 +42,15 @@ def test_render_template_with_context_keys(pipeline, experiment_session):
     )
 
 
-@pytest.mark.django_db()
-def test_render_template_undefined_variable_error(pipeline, experiment_session):
+def test_render_template_undefined_variable_error():
+    experiment_session = ExperimentSessionFactory.build()
     state = PipelineState(
         experiment_session=experiment_session,
         messages=["hello"],
         outputs={},
     )
     node = RenderTemplate(name="test", node_id="123", django_node=None, template_string="{{ nonexistent_var }}")
-    config = {"configurable": {"repo": ORMRepository(session=experiment_session)}}
+    config = {"configurable": {"repo": InMemoryPipelineRepository(session=experiment_session)}}
 
     with pytest.raises(PipelineNodeRunError, match=r'UndefinedError in field "template_string"'):
         node.process(incoming_nodes=[], outgoing_nodes=[], state=state, config=config)
